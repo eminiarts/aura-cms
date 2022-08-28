1039abe721e1a44a7c838febcaa1b3776750934e
Bajram Emini <bajram@eminiarts.ch>
2022-08-28 10:00:05 +0200

    Update aura.php

--- config/aura.php
+++ config/aura.php
@@ -2,5 +2,90 @@
 
 // config for Eminiarts/Aura
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Aura Path
+    |--------------------------------------------------------------------------
+    |
+    | The default is `admin` but you can change it to whatever works best and
+    | doesn't conflict with the routing in your application.
+    |
+    */
 
+    'path' => env('AURA_PATH', 'admin'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Aura Core Path
+    |--------------------------------------------------------------------------
+    |
+    | This is the path which AURA will use to load its core routes and assets.
+    | You may change it if it conflicts with your other routes.
+    |
+    */
+
+    'core_path' => env('AURA_CORE_PATH', 'aura'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Aura Domain
+    |--------------------------------------------------------------------------
+    |
+    | You may change the domain where AURA should be active. If the domain
+    | is empty, all domains will be valid.
+    |
+    */
+
+    'domain' => env('AURA_DOMAIN'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Resources
+    |--------------------------------------------------------------------------
+    |
+    | This is the namespace and directory that Aura will automatically
+    | register resources from. You may also register resources here.
+    |
+    */
+
+    'resources' => [
+        'namespace' => 'App\\Aura\\Resources',
+        'path' => app_path('Aura/Resources'),
+        'register' => [],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Widgets
+    |--------------------------------------------------------------------------
+    |
+    | This is the namespace and directory that Aura will automatically
+    | register dashboard widgets from. You may also register widgets here.
+    |
+    */
+
+    'widgets' => [
+        'namespace' => 'App\\Aura\\Widgets',
+        'path' => app_path('Aura/Widgets'),
+        'register' => [
+            // Widgets\AccountWidget::class,
+            // Widgets\FilamentInfoWidget::class,
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Livewire
+    |--------------------------------------------------------------------------
+    |
+    | This is the namespace and directory that Aura will automatically
+    | register Livewire components inside.
+    |
+    */
+
+
+    'livewire' => [
+        'namespace' => 'App\\Aura',
+        'path' => app_path('Aura'),
+    ],
 ];
