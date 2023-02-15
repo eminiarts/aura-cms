9e9fe53d7405957647a4d7755a6daaf001bab632
Bajram Emini <bajram@eminiarts.ch>
2023-02-15 22:15:39 +0100

    Placeholder

--- resources/views/components/fields/text.blade.php
+++ resources/views/components/fields/text.blade.php
@@ -1,3 +1,3 @@
 <x-aura::fields.wrapper :field="$field">
-    <x-aura::input.text :disabled="optional($field)['disabled']" wire:model.defer="post.fields.{{ optional($field)['slug'] }}" error="post.fields.{{ optional($field)['slug'] }}" placeholder="{{ optional($field)['name'] }}" id="post-field-{{ optional($field)['slug'] }}"></x-aura::input.text>
+    <x-aura::input.text :disabled="optional($field)['disabled']" wire:model.defer="post.fields.{{ optional($field)['slug'] }}" error="post.fields.{{ optional($field)['slug'] }}" placeholder="{{ optional($field)['placeholder'] ?? optional($field)['name'] }}" id="post-field-{{ optional($field)['slug'] }}"></x-aura::input.text>
 </x-aura::fields.wrapper>
