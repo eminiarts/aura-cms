d6c832123c186c868e84945222d41f6882bfc797
Bajram Emini <bajram@eminiarts.ch>
2023-02-18 19:09:53 +0100

    Update InviteUserTest.php

--- tests/Feature/Team/InviteUserTest.php
+++ tests/Feature/Team/InviteUserTest.php
@@ -1,11 +1,12 @@
 <?php
 
-use Eminiarts\Aura\Http\Livewire\Table\Table;
-use Eminiarts\Aura\Http\Livewire\User\InviteUser;
+use Livewire\Livewire;
 use Eminiarts\Aura\Models\User;
 use Eminiarts\Aura\Resources\Post;
+use Eminiarts\Aura\Resources\Role;
+use Eminiarts\Aura\Http\Livewire\Table\Table;
+use Eminiarts\Aura\Http\Livewire\User\InviteUser;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Livewire\Livewire;
 
 // Before each test, create a Superadmin and login
 beforeEach(function () {
@@ -27,15 +28,25 @@ beforeEach(function () {
 test('user can be invited', function () {
     // Test InviteUser Livewire Component
     $component = Livewire::test(InviteUser::class, ['resource' => 'user'])
-        ->call('save')
-        ->assertHasErrors(['post.fields.email' => 'required'])
-        ->set('post.fields.email', 'test@test.ch')
-        ->call('save')
-        ->assertHasErrors(['post.fields.role' => 'required'])
-        ->set('post.fields.role', 1)
-        ->call('save')
-        ->assertHasNoErrors();
+    ->call('save')
+    ->assertHasErrors(['post.fields.email' => 'required'])
+    ->set('post.fields.email', 'test@test.ch')
+    ->call('save')
+    ->assertHasErrors(['post.fields.role' => 'required'])
+    ->set('post.fields.role', 1)
+    ->call('save')
+    ->assertHasNoErrors();
 });
 
 test('user gets correct role', function () {
+});
+
+
+test('Team Invitation can be created', function () {
+    $team = $this->user->currentTeam;
+
+    $invitation = $team->teamInvitations()->create([
+        'email' => 'test@test.ch',
+        'role' => Role::first()->id,
+    ]);
 });
