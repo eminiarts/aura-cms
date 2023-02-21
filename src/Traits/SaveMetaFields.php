98737c5ff2fb8e9ba299bdcebe2b56ea4b2f459e
Bajram Emini <bajram@eminiarts.ch>
2023-02-21 11:23:50 +0100

    Update SaveMetaFields.php

--- src/Traits/SaveMetaFields.php
+++ src/Traits/SaveMetaFields.php
@@ -15,13 +15,7 @@ trait SaveMetaFields
 
                 $post->saveMetaFields($post->attributes['fields']);
 
-                unset($post->attributes['fields']);
-            }
-        });
-
-        static::saved(function ($post) {
-            if (isset($post->metaFields)) {
-                foreach ($post->metaFields as $key => $value) {
+                foreach ($post->attributes['fields'] as $key => $value) {
                     $class = $post->fieldClassBySlug($key);
 
                     // Do not continue if the Field is not found
@@ -55,9 +49,51 @@ trait SaveMetaFields
                     // If the field exists in the $post->getBaseFillable(), it should be safed in the table instead of the meta table
                     if (in_array($key, $post->getBaseFillable())) {
                         $post->attributes[$key] = $value;
+                    }
+                }
+
+                unset($post->attributes['fields']);
+            }
+        });
+
+        static::saved(function ($post) {
+            if (isset($post->metaFields)) {
+                // if ($post->type == 'SlugModel') {
+                //     dump('saved', $post->toArray());
+                // }
+
+
+                foreach ($post->metaFields as $key => $value) {
+                    $class = $post->fieldClassBySlug($key);
+
+                    // Do not continue if the Field is not found
+                    if (! $class) {
+                        continue;
+                    }
+
+                    // if there is a function set{Slug}Field on the model, use it
+                    $method = 'set'.Str::studly($key).'Field';
+
+                    if (method_exists($post, $method)) {
+                        $post = $post->{$method}($value);
 
                         continue;
                     }
+
+                    // If the $class is a Password Field and the value is null, continue
+                    if ($class instanceof \Eminiarts\Aura\Fields\Password && is_null($value)) {
+                        // If the password is available in the $post->attributes, unset it
+                        if (isset($post->attributes[$key])) {
+                            unset($post->attributes[$key]);
+                        }
+
+                        continue;
+                    }
+
+                    if (method_exists($class, 'set')) {
+                        $value = $class->set($value);
+                    }
+
 
 
                     // If there is no ID
@@ -77,6 +113,11 @@ trait SaveMetaFields
 
                 // Reload relation
                 $post->load('meta');
+
+
+                // if ($post->type == 'SlugModel') {
+                //     dump('saved', $post->toArray());
+                // }
             }
         });
     }
