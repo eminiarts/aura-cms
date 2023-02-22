0bf1de819885a1e4a924607bea7358c77faa81b5
Bajram Emini <bajram@eminiarts.ch>
2023-02-23 00:18:46 +0100

    Update MakePosttypeCommandTest.php

--- tests/Feature/Aura/MakePosttypeCommandTest.php
+++ tests/Feature/Aura/MakePosttypeCommandTest.php
@@ -1,10 +1,7 @@
 <?php
 
-use Eminiarts\Aura\Facades\Aura;
-use Eminiarts\Aura\Models\Post;
 use Eminiarts\Aura\Resources\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\DB;
 
 uses(RefreshDatabase::class);
 
@@ -12,44 +9,23 @@ uses()->group('current');
 
 beforeEach(fn () => $this->actingAs($this->user = User::factory()->create()));
 
-
 use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Str;
-use Tests\TestCase;
+use Illuminate\Support\Facades\Storage;
 
-test('it creates a new Aura Posttype', function () {
-    // Arrange
-    $posttypeName = 'TestPosttype';
-    $namespace = 'Eminiarts\Aura\Resources';
-    $filePath = app_path("{$namespace}/{$posttypeName}.php");
+it('generates the correct files for a posttype', function () {
+    Storage::fake('app', [
+                'driver' => 'local',
+                'root' => app_path(),
+            ]);
 
-    // Act
-    Artisan::call('aura:posttype', ['name' => $posttypeName]);
 
-    // Assert
-    expect(File::exists($filePath))->toBeTrue();
-    $content = File::get($filePath);
-    expect(Str::contains($content, "class {$posttypeName}"))->toBeTrue();
-    expect(Str::contains($content, "protected \$type = 'Posttype'"))->toBeTrue();
-    expect(Str::contains($content, "PostName"))->toBeFalse();
-    expect(Str::contains($content, "PostSlug"))->toBeFalse();
-});
+    Artisan::call('aura:posttype', [
+        'name' => 'TestPosttype',
+    ]);
 
-test('it replaces placeholders in the stub file', function () {
-    // Arrange
-    $posttypeName = 'TestPosttype';
-    $namespace = 'Eminiarts\Aura\Resources';
-    $filePath = app_path("{$namespace}/{$posttypeName}.php");
+    dd(Storage::disk('app')->allFiles());
 
-    // Act
-    Artisan::call('aura:posttype', ['name' => $posttypeName]);
 
-    // Assert
-    expect(File::exists($filePath))->toBeTrue();
-    $content = File::get($filePath);
-    expect(Str::contains($content, "class {$posttypeName}"))->toBeTrue();
-    expect(Str::contains($content, "protected \$type = 'Posttype'"))->toBeTrue();
-    expect(Str::contains($content, "TestPosttype"))->toBeTrue();
-    expect(Str::contains($content, Str::slug('TestPosttype')))->toBeTrue();
+    // assert app/Aura/Resources/TestPosttype.php exists
+    $this->assertTrue(Storage::disk('app')->exists('Aura/Resources/TestPosttype.php'));
 });
