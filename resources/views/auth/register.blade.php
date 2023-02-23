837fc87060274d7d8db242d16b6b0b4751f39ad3
Bajram Emini <bajram@eminiarts.ch>
2023-02-23 11:42:10 +0100

    Team Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,15 +4,22 @@
 
         <!-- Name -->
         <div>
+            <x-aura::input-label for="team" :value="__('Team')" />
+            <x-aura::text-input id="team" class="block mt-1 w-full bg-gray-800" type="text" name="team" :value="old('name')" required autofocus />
+            <x-aura::input-error :messages="$errors->get('team')" class="mt-2" />
+        </div>
+     
+        <!-- Name -->
+        <div class="mt-4">
             <x-aura::input-label for="name" :value="__('Name')" />
-            <x-aura::text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+            <x-aura::text-input id="name" class="block mt-1 w-full bg-gray-800" type="text" name="name" :value="old('name')" required />
             <x-aura::input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
             <x-aura::input-label for="email" :value="__('Email')" />
-            <x-aura::text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+            <x-aura::text-input id="email" class="block mt-1 w-full bg-gray-800" type="email" name="email" :value="old('email')" required />
             <x-aura::input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -20,7 +27,7 @@
         <div class="mt-4">
             <x-aura::input-label for="password" :value="__('Password')" />
 
-            <x-aura::text-input id="password" class="block mt-1 w-full"
+            <x-aura::text-input id="password" class="block mt-1 w-full bg-gray-800"
                             type="password"
                             name="password"
                             required autocomplete="new-password" />
@@ -32,7 +39,7 @@
         <div class="mt-4">
             <x-aura::input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-aura::text-input id="password_confirmation" class="block mt-1 w-full"
+            <x-aura::text-input id="password_confirmation" class="block mt-1 w-full bg-gray-800"
                             type="password"
                             name="password_confirmation" required />
 
