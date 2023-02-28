8863dcdec3924d04bbd4bb3700ff740a3b3f2d1e
Enes Emini <enes.emini@gmail.com>
2023-02-28 09:52:51 +0100

    Fixed Posttype Editor without Tabs

---
+++ src/Templates/PanelWithSidebar.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Eminiarts\Aura\Templates;
+
+class PanelWithSidebar
+{
+    public string $name = 'PanelWithSidebar';
+
+    public function getFields()
+    {
+        return [
+            [
+                'name' => 'Panel 1',
+                'type' => 'Eminiarts\\Aura\\Fields\\Panel',
+                'validation' => '',
+                'on_index' => true,
+                'has_conditional_logic' => false,
+                'conditional_logic' => [
+                ],
+                'slug' => 'panel-1',
+                'style' => [
+                    'width' => '70',
+                ],
+            ],
+            [
+                'name' => 'Text 1',
+                'type' => 'Eminiarts\\Aura\\Fields\\Text',
+                'validation' => 'required',
+                'on_index' => true,
+                'has_conditional_logic' => false,
+                'conditional_logic' => [
+                ],
+                'slug' => 'text-1',
+                'style' => [
+                    'width' => '100',
+                ],
+            ],
+
+            [
+                'name' => 'Panel 2',
+                'type' => 'Eminiarts\\Aura\\Fields\\Panel',
+                'validation' => '',
+                'on_index' => true,
+                'has_conditional_logic' => false,
+                'conditional_logic' => [
+                ],
+                'slug' => 'panel-2',
+                'style' => [
+                    'width' => '30',
+                ],
+            ],
+            [
+                'name' => 'Text 2',
+                'type' => 'Eminiarts\\Aura\\Fields\\Text',
+                'validation' => 'required',
+                'on_index' => true,
+                'has_conditional_logic' => false,
+                'conditional_logic' => [
+                ],
+                'slug' => 'text-2',
+                'style' => [
+                    'width' => '100',
+                ],
+            ],
+        ];
+    }
+}
