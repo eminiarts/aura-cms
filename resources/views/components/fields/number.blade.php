9d692258e1a06e6fb7fd729e5a2ba5268dd3c5a6
Enes Emini <enes.emini@gmail.com>
2023-02-28 09:39:08 +0100

    Fix Color Field Console Errors

--- resources/views/components/fields/number.blade.php
+++ resources/views/components/fields/number.blade.php
@@ -1,4 +1,10 @@
 <x-aura::fields.wrapper :field="$field">
-    <x-aura::input.text :disabled="optional($field)['disabled']" suffix="{{ optional($field)['suffix'] }}" prefix="{{ optional($field)['prefix'] }}" type="number" wire:model.defer="post.fields.{{ optional($field)['slug'] }}"
-        error="post.fields.{{ optional($field)['slug'] }}"></x-aura::input.text>
+    <x-aura::input.text
+        :disabled="optional($field)['disabled']"
+        suffix="{{ optional($field)['suffix'] }}"
+        prefix="{{ optional($field)['prefix'] }}"
+        type="number"
+        wire:model.defer="post.fields.{{ optional($field)['slug'] }}"
+        error="post.fields.{{ optional($field)['slug'] }}"
+    ></x-aura::input.text>
 </x-aura::fields.wrapper>
