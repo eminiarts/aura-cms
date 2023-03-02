74c65fec116b9d714a170dfa66775baa7ab2cecf
Enes Emini <enes.emini@gmail.com>
2023-03-02 20:43:36 +0100

    WIP

--- resources/views/livewire/post/index.blade.php
+++ resources/views/livewire/post/index.blade.php
@@ -1,12 +1,12 @@
 <div>
     @section('title', 'All ' . Str::plural($slug) . ' • ')
-    
-    <div class="flex items-center justify-between">
+
+    <div class="flex items-start justify-between">
         <x-aura::breadcrumbs>
             <x-aura::breadcrumbs.li :href="route('aura.dashboard')" title="" icon="dashboard" iconClass="text-gray-500 w-7 h-7 mr-0" />
             <x-aura::breadcrumbs.li :title="Str::plural($slug)"  />
         </x-aura::breadcrumbs.li>
-        
+
         <div>
             @if($post->isAppResource())
             <x-aura::button.transparent route="aura.posttype.edit" :id="$slug" size="" >
@@ -16,10 +16,10 @@
             @endif
         </div>
     </div>
-    
+
     @if($widgets = $post->getWidgets())
     <x-aura::widgets :widgets="$widgets" />
     @endif
-    
+
     <livewire:aura::table :model="$post"/>
 </div>
