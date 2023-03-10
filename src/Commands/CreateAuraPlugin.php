3b3344729da795d5d7382acc3a59cb68f52d5d66
Bajram Emini <bajram@eminiarts.ch>
2023-03-10 16:13:41 +0100

    Update CreateAuraPlugin.php

--- src/Commands/CreateAuraPlugin.php
+++ src/Commands/CreateAuraPlugin.php
@@ -24,56 +24,59 @@ class CreateAuraPlugin extends Command
 
         $pluginType = $this->choice('Select the type of plugin you want to create', $options);
 
-        dd($vendor, $name, $pluginType);
-
-        $pluginDirectory = app_path("Aura/Plugins/{$vendor}/{$name}");
-
-        File::makeDirectory($pluginDirectory, 0755, true);
-
-        $composerJson = [
-            'name' => "{$vendor}/{$name}",
-            'description' => '',
-            'type' => 'laravel-package',
-            'license' => 'MIT',
-            'authors' => [
-                [
-                    'name' => '',
-                    'email' => '',
-                ],
-            ],
-            'autoload' => [
-                'psr-4' => [
-                    "{$vendor}\\{$name}\\" => "src/",
-                ],
-            ],
-            'require' => [
-                'illuminate/support' => '^8.0',
-            ],
-            'extra' => [
-                'laravel' => [
-                    'providers' => [
-                        "{$vendor}\\{$name}\\{$name}ServiceProvider",
-                    ],
-                ],
-            ],
+        $folder = [
+            'Complete plugin' => 'plugin',
+            'Posttype plugin' => 'plugin-posttype',
+            'Field plugin' => 'plugin-field',
+            'Widget plugin' => 'plugin-widget',
         ];
 
-        File::put("{$pluginDirectory}/composer.json", json_encode($composerJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+        $pluginDirectory = app_path("Aura/Plugins/{$vendor}/{$name}");
+        File::makeDirectory($pluginDirectory, 0755, true);
 
-        $providerClassName = "{$name}ServiceProvider";
-        $providerStub = File::get(__DIR__."/stubs/{$pluginType}/ServiceProvider.stub");
-        $providerContent = str_replace('{{providerClassName}}', $providerClassName, $providerStub);
-        File::put("{$pluginDirectory}/{$providerClassName}.php", $providerContent);
+        $stubDirectory = $this->getStubsDirectory($folder[$pluginType]);
+        
+        File::copyDirectory($stubDirectory, $pluginDirectory);
 
         $this->info("{$pluginType} plugin created at {$pluginDirectory}");
 
-        if ($this->confirm("Do you want to append {$providerClassName} to config/app.php?")) {
+        dd('stop', $pluginDirectory, $stubDirectory, $vendor, $name);
+
+        $this->info("Replacing placeholders...");
+        $this->runProcess("php {$pluginDirectory}/configure.php --vendor={$vendor} --name={$name}");
+
+        if ($this->confirm("Do you want to append {$name}ServiceProvider to config/app.php?")) {
+            $providerClassName = "{$name}ServiceProvider";
             $configFile = base_path('config/app.php');
             $configContent = File::get($configFile);
-            $newProvider = "{$vendor}\\{$name}\\{$name}ServiceProvider::class";
+            $newProvider = "{$vendor}\\{$name}\\{$providerClassName}::class";
             $configContent = str_replace("/* providers */", "/* providers */\n        {$newProvider},", $configContent);
             File::put($configFile, $configContent);
             $this->info("{$providerClassName} added to config/app.php");
         }
+    }
+
+    public function getStubsDirectory($path)
+    {
+        return __DIR__.'/../../stubs/' . $path;
+    }
+
+    private function runProcess($command)
+    {
+        $process = proc_open($command, [
+            0 => STDIN,
+            1 => STDOUT,
+            2 => STDERR,
+        ], $pipes);
+
+        if (is_resource($process)) {
+            fclose($pipes[0]);
+            fclose($pipes[1]);
+            fclose($pipes[2]);
+
+            return proc_close($process);
+        }
+
+        return -1;
     }
 }
