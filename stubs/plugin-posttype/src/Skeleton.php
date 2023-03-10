c9246129dc6896f98c19229d61c14d0b8be4d15b
Bajram Emini <bajram@eminiarts.ch>
2023-03-10 18:08:06 +0100

    WIP Posttype Plugin

--- stubs/plugin-posttype/src/Skeleton.php
+++ stubs/plugin-posttype/src/Skeleton.php
@@ -6,5 +6,35 @@ use Eminiarts\Aura\Models\Post;
 
 class Skeleton extends Post
 {
-    
+    public static string $type = 'Skeleton';
+
+    public static ?string $slug = 'skeleton';
+
+    protected static ?string $group = 'VendorName';
+
+    public static function getWidgets(): array
+    {
+        return [];
+    }
+
+    public function getIcon()
+    {
+        return '<svg class="w-5 h-5" viewBox="0 0 18 18" fill="none" stroke="currentColor" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15.75 9a6.75 6.75 0 1 1-13.5 0 6.75 6.75 0 0 1 13.5 0Z" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+    }
+
+    public static function getFields()
+    {
+        return [
+            [
+                'name' => 'Title',
+                'type' => 'Eminiarts\\Aura\\Fields\\Text',
+                'validation' => 'required',
+                'on_index' => true,
+                'slug' => 'title',
+                'style' => [
+                    'width' => '100',
+                ],
+            ],
+        ];
+    }
 }
