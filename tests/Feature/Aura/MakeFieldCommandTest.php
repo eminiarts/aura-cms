cf4feb06c6abae5c7e07304f66f632edf5a346b1
Bajram Emini <bajram@eminiarts.ch>
2023-03-11 23:22:06 +0100

    Make Field Command

--- tests/Feature/Aura/MakeFieldCommandTest.php
+++ tests/Feature/Aura/MakeFieldCommandTest.php
@@ -16,33 +16,34 @@ it('generates the correct files for a field', function () {
         'name' => 'TestField',
     ]);
 
-    // assert app/Aura/Resources/TestField.php exists
-    $this->assertTrue(File::exists(app_path('Aura/Resources/TestField.php')));
+    // assert app/Aura/Fields/TestField.php exists
+    $this->assertTrue(File::exists(app_path('Aura/Fields/TestField.php')));
 
-    // delete app/Aura/Resources/TestField.php
-    File::delete(app_path('Aura/Resources/TestField.php'));
+    // delete app/Aura/Fields/TestField.php
+    File::delete(app_path('Aura/Fields/TestField.php'));
 });
 
 test('field command and contents', function () {
-    $fieldName = 'field';
+    $fieldName = 'TestField';
     
     // Run the command and provide input
     $this->artisan('aura:field ' . $fieldName)->assertExitCode(0);
 
     // Assert that the field class file was generated
-    expect(File::exists(app_path('Aura/Resources/' . ucfirst($fieldName) . '.php')))->toBeTrue();
+    expect(File::exists(app_path('Aura/Fields/' . ucfirst($fieldName) . '.php')))->toBeTrue();
 
     // Check file contents
-    $fieldClass = File::get(app_path('Aura/Resources/' . ucfirst($fieldName) . '.php'));
+    $fieldClass = File::get(app_path('Aura/Fields/' . ucfirst($fieldName) . '.php'));
 
-    expect($fieldClass)->toContain("namespace App\Aura\Resources;");
-    expect($fieldClass)->toContain('public static string $type = \'Field\';');
-    expect($fieldClass)->toContain('public static ?string $slug = \'field\';');
-    expect($fieldClass)->toContain("public function getIcon()");
-    expect($fieldClass)->toContain("public static function getFields()");
+    expect($fieldClass)->toContain("namespace App\Aura\Fields;");
+    expect($fieldClass)->toContain('use Eminiarts\Aura\Fields\Field;');
+    expect($fieldClass)->toContain('class TestField extends Field');
+    expect($fieldClass)->toContain('public $component = \'fields.testfield\';');
+    expect($fieldClass)->toContain('public $view = \'fields.testfield-view\';');
+    expect($fieldClass)->toContain("public function getFields()");
 
     // Delete the field class file
-    if (File::exists(app_path('Aura/Resources/' . ucfirst($fieldName) . '.php'))) {
-        File::delete(app_path('Aura/Resources/' . ucfirst($fieldName) . '.php'));
+    if (File::exists(app_path('Aura/Fields/' . ucfirst($fieldName) . '.php'))) {
+        File::delete(app_path('Aura/Fields/' . ucfirst($fieldName) . '.php'));
     }
 });
