216adf9e2a7ca2d50437aa7c70211ff6acb15456
Enes Emini <enes.emini@gmail.com>
2023-03-12 21:44:00 +0100

    Fix Modal Max W

--- resources/views/livewire/media-manager.blade.php
+++ resources/views/livewire/media-manager.blade.php
@@ -1,9 +1,22 @@
 <div class="w-full p-8">
-
     <div class="">
-        <h1>Modal</h1>
         <livewire:aura::table :model="app('Eminiarts\Aura\Resources\Attachment')"/>
     </div>
+
+    {{--
+        $maxWidths = [
+        'sm'  => 'sm:max-w-sm',
+        'md'  => 'sm:max-w-md',
+        'lg'  => 'sm:max-w-md md:max-w-lg',
+        'xl'  => 'sm:max-w-md md:max-w-xl',
+        '2xl' => 'sm:max-w-md md:max-w-xl lg:max-w-2xl',
+        '3xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl',
+        '4xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-4xl',
+        '5xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl',
+        '6xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-6xl',
+        '7xl' => 'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-7xl',
+    ]
+        --}}
 
     {{-- Footer with 2 buttons: close and select --}}
     <div class="flex justify-end mt-4">
