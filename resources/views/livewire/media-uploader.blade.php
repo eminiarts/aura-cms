cb8dffd57d13a2a49626b6c0e3f7e78ac8a8a053
Bajram Emini <bajram@eminiarts.ch>
2023-03-14 23:39:20 +0100

    WIP

--- resources/views/livewire/media-uploader.blade.php
+++ resources/views/livewire/media-uploader.blade.php
@@ -62,36 +62,60 @@
             },
         }">
 
-        <div class="" 
-            x-on:drop="isDropping = false" 
-            x-on:drop.prevent="handleFileDrop($event)"
-            x-on:dragover.prevent="isDropping = true; console.log('dragover'); dragover($event);"
-            x-on:dragleave.prevent="isDropping = false; console.log('dragleave');"
-        >
-        
-            <div class="flex items-center justify-center w-full mb-4" x-cloak>
-                <div class="absolute top-0 bottom-0 left-0 right-0 z-30 flex items-center justify-center bg-primary-400 opacity-90"
+            <div class="" x-on:drop="isDropping = false" x-on:drop.prevent="handleFileDrop($event)"
+                x-on:dragover.prevent="isDropping = true; console.log('dragover'); dragover($event);"
+                x-on:dragleave.prevent="isDropping = false; console.log('dragleave');">
+
+                <div class="flex items-center justify-center w-full mb-4" x-cloak>
+                    <div class="absolute top-0 bottom-0 left-0 right-0 z-30 flex items-center justify-center bg-primary-400 opacity-90"
                         x-show="isDropping">
                         <span class="text-3xl text-white">Release file to upload!</span>
                     </div>
 
-                    
-                    
-            </div>
 
-            <div class="bg-white dark:bg-gray-900 h-[4px] w-full mt-0">
-                <div class="bg-primary-500 h-[4px]" style="transition: width 0.5s" :style="`width: ${progress}%;`"
-                    x-show="isUploading">
                 </div>
-            </div>
 
-            <livewire:aura::table :model="$post"/>
+                <div class="bg-white dark:bg-gray-900 h-[4px] w-full mt-0">
+                    <div class="bg-primary-500 h-[4px]" style="transition: width 0.5s" :style="`width: ${progress}%;`"
+                        x-show="isUploading">
+                    </div>
+                </div>
+
+                <div class="flex items-center justify-between mt-6">
+                    <div>
+                        <h1 class="text-3xl font-semibold">Attachments</h1>
+
+
+                        </h3>
+                    </div>
+
+                    <div>
+                        <div>
+
+                            <div>
+                                <label for="file-upload">
+                                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
+                                            class="font-semibold">Click to
+                                            upload</span> or drag and drop</p>
+
+                                    <input type="file" id="file-upload" multiple @change="handleFileSelect"
+                                        class="hidden" wire:model.defer="media" />
+                                </label>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <livewire:aura::table :model="$post" />
 
             </div>
 
         </div>
     </div>
 
-   
+
 
 </div>
