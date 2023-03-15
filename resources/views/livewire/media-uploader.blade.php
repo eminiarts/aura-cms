abf27a4faa49a7a2e976b49d6f1fe4306de9778f
Bajram Emini <bajram@eminiarts.ch>
2023-03-15 16:56:32 +0100

    WIP

--- resources/views/livewire/media-uploader.blade.php
+++ resources/views/livewire/media-uploader.blade.php
@@ -1,26 +1,61 @@
 <div>
     <div>
         @if($media && count($media))
+
+        {{-- @dump($media, json_encode($media)) --}}
     <div
     x-data="{ media: {{ json_encode($media) }}, loading: true }"
     class="fixed top-0 right-0 p-4 space-y-4 z-50"
 >
-    <template x-for="(file, index) in media" :key="index">
+    @foreach($media as $file)
+    <div aria-live="assertive" class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6">
+  <div class="flex w-full flex-col items-end space-y-4 sm:items-end">
+    <!--
+      Notification panel, dynamically insert this into the live region when it needs to be displayed
+
+      Entering: "transform ease-out duration-300 transition"
+        From: "translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
+        To: "translate-y-0 opacity-100 sm:translate-x-0"
+      Leaving: "transition ease-in duration-100"
+        From: "opacity-100"
+        To: "opacity-0"
+    -->
+    <div class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+      <div class="p-4">
+        <div class="flex items-start">
+          <div class="flex-shrink-0">
+            <svg class="h-6 w-6 text-green-400" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+          </div>
+          <div class="ml-3 w-0 flex-1 pt-0.5">
+            <p class="text-sm font-medium text-gray-900">Successfully saved!</p>
+            <p class="mt-1 text-sm text-gray-500">Anyone with a link can now view this file.</p>
+          </div>
+          <div class="ml-4 flex flex-shrink-0">
+            <button type="button" class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+              <span class="sr-only">Close</span>
+              <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z" />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
         <div
             x-show="loading"
-            x-init="setTimeout(() => { loading = false }, 5000); console.log(file);"
-            class="flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+            x-init="setTimeout(() => { loading = false }, 3000)"
+            class="relative flex items-center w-full max-w-xs p-4 mb-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
             role="alert"
         >
             <div class="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-green-500 bg-green-100 rounded-lg dark:bg-green-800 dark:text-green-200">
                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
             </div>
-            <div class="ml-3 text-sm font-normal">
-                @verbatim
-                    {{ file.original_name }} uploaded
-                @endverbatim
-            </div>
-            <div class="w-full h-1.5 mt-2">
+            <div class="ml-3 text-sm font-normal">{{ $file->getClientOriginalName() }} uploaded</div>
+            <div class="absolute bottom-0 left-0 w-full h-1.5">
                 <div
                     x-show="loading"
                     x-transition:leave="transition ease-linear duration-1000"
@@ -30,7 +65,7 @@
                 ></div>
             </div>
         </div>
-    </template>
+    @endforeach
 </div>
 
     @endif
