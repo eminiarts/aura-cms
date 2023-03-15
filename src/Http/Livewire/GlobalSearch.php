311a765b6c8473e98918dc02ab82b538841e7f4c
Enes Emini <enes.emini@gmail.com>
2023-03-16 00:07:47 +0100

    WIP GlobalSearch

--- src/Http/Livewire/GlobalSearch.php
+++ src/Http/Livewire/GlobalSearch.php
@@ -3,6 +3,7 @@
 namespace Eminiarts\Aura\Http\Livewire;
 
 use Livewire\Component;
+use Eminiarts\Aura\Models\User;
 use Eminiarts\Aura\Resources\Post;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 
@@ -10,17 +11,53 @@ class GlobalSearch extends Component
 {
     public $search;
 
+
     public function render()
     {
         $searchResults = [];
 
-        if (!empty($this->search)) {
-            $searchResults = Post::where('title', 'LIKE', '%' . $this->search . '%')->orWhereHas('meta', function (Builder $query) {
+        // Get all accessible resources
+        $resources = app('aura')::getResources();
+
+        // Initialize search results array
+
+        // filter out flows and flow_logs from resources
+        $resources = array_filter($resources, function ($resource) {
+            // dump($resource::getSlug());
+            return $resource::getSlug() !== 'flow' && $resource::getSlug() !== 'flowlog' && $resource::getSlug() !== 'operation' && $resource::getSlug() !== 'flowoperation' && $resource::getSlug() !== 'operationlog' && $resource::getSlug() !== 'option' && $resource::getSlug() !== 'team' && $resource::getSlug() !== 'user' && $resource::getSlug() !== 'product' ;
+        });
+
+
+        // dd($resources);
+
+        // if $this->search is empty, return
+        if (!$this->search) {
+            return view('aura::livewire.global-search', [
+                'searchResults' => $searchResults,
+            ]);
+        }
+
+        // Search in each resource model
+        foreach ($resources as $resource) {
+            $model = $resource::query();
+
+            // if model has table, return
+            // if ($model->table) {
+            //     continue;
+            // }
+
+            $results = $model->where('title', 'like', '%' . $this->search . '%')->orWhereHas('meta', function (Builder $query) {
                 $query->where('key', 'LIKE', '%' . $this->search . '%')
                     ->orWhere('value', 'LIKE', '%' . $this->search . '%');
             })->get();
-            // Implement logic for advanced search, using natural language processing, post_meta, post_type, etc.
+            $searchResults = array_merge($searchResults, $results->toArray());
         }
+
+        // Search in User model
+        $userResults = User::where('name', 'like', '%' . $this->search . '%')
+            ->orWhere('email', 'like', '%' . $this->search . '%')
+            ->get();
+        $searchResults = array_merge($searchResults, $userResults->toArray());
 
         return view('aura::livewire.global-search', [
             'searchResults' => $searchResults
