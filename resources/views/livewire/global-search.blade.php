48abeff6f9d4e25a3efcfd39c14491c6caba1c7c
Enes Emini <enes.emini@gmail.com>
2023-03-16 17:21:00 +0100

    GlobalSearch WIP

--- resources/views/livewire/global-search.blade.php
+++ resources/views/livewire/global-search.blade.php
@@ -1,4 +1,10 @@
 <div>
+    <style>
+        .search-list-icon svg {
+            width: 1.5rem;
+            height: 1.5rem;
+        }
+    </style>
     <div x-show="show" x-data="globalSearch" x-ref="searchContainer" style="display: none;" @search.window="search()" @keydown.escape.window="closeSearch()">
     <div class="fixed inset-0 z-30">
         <div class="absolute inset-0 bg-black/10 backdrop-blur-sm"></div>
@@ -11,8 +17,10 @@
                         </svg>
                     </div>
 
-                    <input wire:model="search" x-ref="searchField" class="py-4 px-2 focus:outline-none relative block w-full rounded-none rounded-t-md bg-transparent focus:z-10 focus:border-0 focus:!border-none sm:text-sm" style="border: none; box-shadow: none;" aria-autocomplete="list" aria-labelledby="docsearch-label" id="docsearch-input" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" placeholder="Search Aura" maxlength="64" type="search" @keydown.arrow-up.prevent="selectedIndex > 0 ? selectedIndex-- : selectedIndex = searchResults.length - 1"
-                            @keydown.arrow-down.prevent="selectedIndex < searchResults.length - 1 ? selectedIndex++ : selectedIndex = 0"
+                    <input wire:model="search" x-ref="searchField" class="py-4 px-2 focus:outline-none relative block w-full rounded-none rounded-t-md bg-transparent focus:z-10 focus:border-0 focus:!border-none sm:text-sm" style="border: none; box-shadow: none;" aria-autocomplete="list" aria-labelledby="docsearch-label" id="docsearch-input" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" placeholder="Search Aura" maxlength="64" type="search"
+
+                        @keydown.arrow-up.prevent="selectPrevious()"
+                        @keydown.arrow-down.prevent="selectNext()"
                         @keydown.enter="openSelectedResult()"
                     >
 
@@ -24,31 +32,36 @@
                 <div class="p-0 border-t">
                     <div>
 
-                        <ul class="mt-4">
-                            <template x-for="(result, index) in searchResults" :key="index">
-                                <li class="flex px-4 py-2 border-b cursor-default select-none hover:bg-primary-500 hover:text-white" x-bind:id="'option-' + index" role="option" tabindex="-1"
-                                :class="{ 'bg-primary-500 text-white': index === selectedIndex }"
+                        <ul class="mt-4" x-ref="searchList" wire:key="searchList">
+                            @forelse($this->searchResults as $key => $result)
+                                <li
+                                    class="flex px-4 py-2 cursor-default select-none hover:bg-primary-500 hover:text-white" id="option-1" role="option" tabindex="-1"
+                                    wire:key="search-result-{{ $key }}"
+                                    :class="{ 'bg-primary-500 text-white': {{ $key }} === selectedIndex }"
                                 >
-                                    <div class="w-8 h-8 mr-4 rounded-full shrink-0 bg-primary-200">
+
+                                    <div class="flex items-center justify-center w-8 h-8 mr-4 rounded-full search-list-icon text-primary-500 shrink-0 bg-primary-200">
                                         {{-- SVG Icon Circle --}}
-                                        <svg class="w-full h-full text-primary-500" fill="currentColor" viewBox="0 0 24 24">
-                                            <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path>
-                                        </svg>
+                                        {!! $result->getIcon() !!}
                                     </div>
 
                                     <div class="flex-1">
-                                        <a x-bind:href="result.view_url">
-                                            {{-- x-if result is array --}}
-
-                                                <span x-text="result['type']"></span>: <span x-text="result['id']"></span> <span x-text="result['title']"></span>
-
-                                            <template x-if="result.type == 'user'">
-                                                <span x-text="result.type"></span>: <span x-text="result.id"></span> <span x-text="result.name"></span>
-                                            </template>
+                                        <a href="{{ route('aura.post.view', ['slug' => $result->getType(), 'id' => $result->id]) }}">
+                                            {{ $result->getType() }}: {{ $result->id }} {{ $result->title }}
                                         </a>
                                     </div>
+
+                                    <div>
+                                        <svg class="w-4 h-4 ml-auto text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                        </svg>
+                                    </div>
+
                                 </li>
-                            </template>
+                                @empty
+                            @endforelse
+
+
                         </ul>
 
                     </div>
@@ -68,7 +81,6 @@
             search: '',
 
             items: [],
-            searchResults: @entangle('searchResults'),
 
             selectedIndex: -1,
 
@@ -87,8 +99,23 @@
                 this.show = false;
             },
 
-            openSelectedResult() {
-                this.searchResults[this.selectedIndex].view_url && window.location.replace(this.searchResults[this.selectedIndex].view_url);
+            selectPrevious() {
+                var length = this.$refs.searchList.children.length;
+
+                this.selectedIndex > 0 ? this.selectedIndex-- : this.selectedIndex = length - 1;
+            },
+
+            selectNext() {
+                var length = this.$refs.searchList.children.length;
+                this.selectedIndex < length - 1 ? this.selectedIndex++ : this.selectedIndex = 0;
+            },
+
+            openSelectedResult(e) {
+                console.log('openSelectedResult');
+                if (this.selectedIndex > -1) {
+                    var link = this.$refs.searchList.children[this.selectedIndex].querySelector('a');
+                    link.click();
+                }
             }
 
 		}))
--- src/Http/Livewire/GlobalSearch.php
+++ src/Http/Livewire/GlobalSearch.php
@@ -11,16 +11,19 @@ class GlobalSearch extends Component
 {
     public $search;
 
-    public $searchResults = [];
-
 
     public function render()
     {
         return view('aura::livewire.global-search');
     }
 
-    public function updatedSearch()
+    public function getSearchResultsProperty()
     {
+        // if no $this->search return
+        if (!$this->search || $this->search === '') {
+            return [];
+        }
+        // dd('getSearchResultsProperty');
         // Get all accessible resources
         $resources = app('aura')::getResources();
 
@@ -29,51 +32,62 @@ class GlobalSearch extends Component
         // filter out flows and flow_logs from resources
         $resources = array_filter($resources, function ($resource) {
             // dump($resource::getSlug());
-            return $resource::getSlug() !== 'flow' && $resource::getSlug() !== 'flowlog' && $resource::getSlug() !== 'operation' && $resource::getSlug() !== 'flowoperation' && $resource::getSlug() !== 'operationlog' && $resource::getSlug() !== 'option' && $resource::getSlug() !== 'team' && $resource::getSlug() !== 'user' && $resource::getSlug() !== 'product' ;
+            return $resource::getSlug() !== 'resource' && $resource::getSlug() !== 'flow' && $resource::getSlug() !== 'flowlog' && $resource::getSlug() !== 'operation' && $resource::getSlug() !== 'flowoperation' && $resource::getSlug() !== 'operationlog' && $resource::getSlug() !== 'option' && $resource::getSlug() !== 'team' && $resource::getSlug() !== 'user' && $resource::getSlug() !== 'product' ;
         });
 
 
         // dd($resources);
 
-        $this->searchResults = [];
+        $searchResults = collect([]);
 
         // Search in each resource model
         foreach ($resources as $resource) {
             $model = $resource::query();
 
-            // if model has table, return
-            // if ($model->table) {
-            //     continue;
-            // }
-
             $results = $model->where('title', 'like', '%' . $this->search . '%')->orWhereHas('meta', function (Builder $query) {
                 $query->where('key', 'LIKE', '%' . $this->search . '%')
                     ->orWhere('value', 'LIKE', '%' . $this->search . '%');
             })->get();
-            $this->searchResults = array_merge($this->searchResults, $results->toArray());
+
+            $searchResults->push(...$results);
         }
 
         // Search in User model
         $userResults = User::where('name', 'like', '%' . $this->search . '%')
             ->orWhere('email', 'like', '%' . $this->search . '%')
             ->get();
-        $this->searchResults = array_merge($this->searchResults, $userResults->toArray());
+        $searchResults->push(...$userResults);
 
-        // limit search results to 15
-        $this->searchResults = array_slice($this->searchResults, 0, 10);
-        // dump($this->searchResults);
+        // // limit search results to 15
+        // $searchResults = array_slice($searchResults, 0, 10);
+        // dump($searchResults);
         // map searchresults and add View URL to each item
-        // dump($this->searchResults);
+        // dump($searchResults);
 
-        $this->searchResults = array_map(function ($item) {
+        // $searchResults = array_map(function ($item) {
+        //     // return route aura.post.view with slug and id
+        //     if (isset($item['type'])) {
+        //         $item['view_url'] = route('aura.post.view', ['slug' => $item['type'], 'id' => $item['id']]);
+        //     } else {
+        //         $item['view_url'] = route('aura.post.view', ['slug' => 'user', 'id' => $item['id']]);
+        //     }
+        //     return $item;
+        // }, $searchResults);
+
+        // map collection searchresults and add View URL to each item
+        // dump('hello', $searchResults);
+
+        $searchResults = $searchResults->flatten()->map(function ($item) {
             // return route aura.post.view with slug and id
-            if (isset($item['type'])) {
-            $item['view_url'] = route('aura.post.view', ['slug' => $item['type'], 'id' => $item['id']]);
+            if (isset($item->type)) {
+                $item['view_url'] = route('aura.post.view', ['slug' => $item->type, 'id' => $item->id]);
             } else {
-                $item['view_url'] = route('aura.post.view', ['slug' => 'user', 'id' => $item['id']]);
+                $item['view_url'] = route('aura.post.view', ['slug' => 'user', 'id' => $item->id]);
             }
             return $item;
-        }, $this->searchResults);
+        });
+
+        return $searchResults;
     }
 
 
