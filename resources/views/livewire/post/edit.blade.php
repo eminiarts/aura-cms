80ac337b9722a3ef2dc58866f4e3832f532a7bf2
Bajram Emini <bajram@eminiarts.ch>
2023-03-17 21:10:29 +0100

    Attachment

--- resources/views/livewire/post/edit.blade.php
+++ resources/views/livewire/post/edit.blade.php
@@ -46,7 +46,6 @@
         model: @entangle('post').defer,
         init() {
             console.log('init post edit', this.model);
-
         }
     }">
 
@@ -54,14 +53,13 @@
 
             {{-- @dump($this->fields) --}}
             {{-- @dump($this->post) --}}
-
+            <div class="flex flex-wrap items-start -mx-2">
             @foreach($this->editFields as $key => $field)
-            <x-aura::fields.conditions :field="$field" :model="$model">
-                <div wire:key="post-field-{{ $key }}">
+            <x-aura::fields.conditions :field="$field" :model="$model" wire:key="post-field-{{ $key }}">
                     <x-dynamic-component :component="$field['field']->component" :field="$field" />
-                </div>
             </x-aura::fields.conditions>
             @endforeach
+            </div>
 
             @if (count($errors->all()))
             <div class="block">
