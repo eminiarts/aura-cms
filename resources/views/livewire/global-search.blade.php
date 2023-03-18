4544a6b297fdbab4cba81a65064b17c696c7d132
Enes Emini <enes.emini@gmail.com>
2023-03-18 10:10:52 +0100

    WIP Bookmarks

--- resources/views/livewire/global-search.blade.php
+++ resources/views/livewire/global-search.blade.php
@@ -60,8 +60,8 @@
                                                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"></path></svg>
                                             </div>
 
-                                            <div class="flex-1">
-                                                <a :href="page.url" x-text="page.title"></a>
+                                            <div class="flex-1 overflow-hidden whitespace-nowrap text-ellipsis">
+                                                <a class="block overflow-hidden text-ellipsis" :href="page.url" x-text="page.title"></a>
                                             </div>
                                         </li>
                                     </template>
@@ -79,8 +79,8 @@
                                                 <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"></path></svg>
                                             </div>
 
-                                            <div class="flex-1">
-                                                <a href="{{ $bookmark['url'] }}">{{ $bookmark['title'] }}</a>
+                                            <div class="flex-1 overflow-hidden whitespace-nowrap text-ellipsis">
+                                                <a class="block overflow-hidden text-ellipsis"  href="{{ $bookmark['url'] }}">{{ $bookmark['title'] }}</a>
                                             </div>
 
                                             <div>
