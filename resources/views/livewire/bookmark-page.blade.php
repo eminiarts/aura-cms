7d138174f641256f26cd30ddf6bf2be111e05324
Enes Emini <enes.emini@gmail.com>
2023-03-18 10:02:02 +0100

    WIP Bookmarks

--- resources/views/livewire/bookmark-page.blade.php
+++ resources/views/livewire/bookmark-page.blade.php
@@ -1,8 +1,16 @@
 <div>
-    <button wire:click="toggleBookmark" class="focus:outline-none" :class="{'text-primary-600': {{ $this->isBookmarked ? 'true' : 'false' }}, 'text-gray-200': {{ !$this->isBookmarked ? 'true' : 'false' }} }">
-        {{-- svg bookmark --}}
-        <svg class="w-5 h-5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-            <path d="M5 19l7-7 7 7"></path>
-        </svg>
+    <button wire:click="toggleBookmark" class="flex items-center justify-center ml-2 focus:outline-none w-7 h-7" :class="{'text-primary-600': {{ $this->isBookmarked ? 'true' : 'false' }}, 'text-gray-200': {{ !$this->isBookmarked ? 'true' : 'false' }} }">
+
+        @if($this->isBookmarked)
+            <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M9 10.5L11 12.5L15.5 8M19 21V7.8C19 6.11984 19 5.27976 18.673 4.63803C18.3854 4.07354 17.9265 3.6146 17.362 3.32698C16.7202 3 15.8802 3 14.2 3H9.8C8.11984 3 7.27976 3 6.63803 3.32698C6.07354 3.6146 5.6146 4.07354 5.32698 4.63803C5 5.27976 5 6.11984 5 7.8V21L12 17L19 21Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+
+        @else
+            <svg class="w-5 h-5" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 7.8C5 6.11984 5 5.27976 5.32698 4.63803C5.6146 4.07354 6.07354 3.6146 6.63803 3.32698C7.27976 3 8.11984 3 9.8 3H14.2C15.8802 3 16.7202 3 17.362 3.32698C17.9265 3.6146 18.3854 4.07354 18.673 4.63803C19 5.27976 19 6.11984 19 7.8V21L12 17L5 21V7.8Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        @endif
+
     </button>
 </div>
