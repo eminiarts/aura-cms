9517372ec564271d5a9a9f6ca0daf39f4cb80eb0
Bajram Emini <bajram@eminiarts.ch>
2023-03-26 09:47:09 +0200

    Update Taxonomy.php

--- src/Models/Taxonomy.php
+++ src/Models/Taxonomy.php
@@ -2,10 +2,16 @@
 
 namespace Eminiarts\Aura\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Eminiarts\Aura\Models\Scopes\TeamScope;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Taxonomy extends Model
 {
     use HasFactory;
+
+    protected static function booted()
+    {
+        static::addGlobalScope(new TeamScope());
+    }
 }
