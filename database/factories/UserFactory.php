3266a5a08ba957008ceecb4a3726fda8fd5e8004
Bajram Emini <bajram@eminiarts.ch>
2023-03-29 00:03:53 +0200

    Without Team Functionality

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,14 +24,24 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        return [
+        $data = [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
-            'current_team_id' => 1,
         ];
+
+        if (!config()->has('aura.teams')) {
+            dd($data, config()->has('aura.teams'), config('aura.teams'));
+        }
+
+        if (config()->has('aura.teams') && config('aura.teams')) {
+            $data['current_team_id'] = Team::factory()->create()->id;
+        }
+
+
+        return $data;
     }
 
     /**
@@ -55,7 +65,7 @@ class UserFactory extends Factory
      */
     public function withPersonalTeam()
     {
-        if (! Features::hasTeamFeatures()) {
+        if (! config('aura.teams')) {
             return $this->state([]);
         }
 
