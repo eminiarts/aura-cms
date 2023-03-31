dde212ac9f6517a66482c2c87723e56d65370c7d
Bajram Emini <bajram@eminiarts.ch>
2023-03-31 22:11:07 +0200

    Update CreateFieldsTest.php

--- tests/Feature/Resource/CreateFieldsTest.php
+++ tests/Feature/Resource/CreateFieldsTest.php
@@ -1,6 +1,12 @@
 <?php
 
 use Eminiarts\Aura\Resource;
+use Eminiarts\Aura\Pipeline\ApplyTabs;
+use Eminiarts\Aura\Pipeline\MapFields;
+use Eminiarts\Aura\Pipeline\AddIdsToFields;
+use Eminiarts\Aura\Pipeline\FilterCreateFields;
+use Eminiarts\Aura\Pipeline\ApplyParentConditionalLogic;
+use Eminiarts\Aura\Pipeline\ApplyParentDisplayAttributes;
 
 // current
 uses()->group('current');
@@ -63,4 +69,26 @@ test('if the first tab is hidden, tabs should be applied correctly to second tab
 
     // $fields[0]['fields'][0]['fields'] should have 1 field
     expect(count($fields[0]['fields'][0]['fields']))->toBe(1);
+});
+
+test('check on_create inheritance', function () {
+    $model = new CreateFieldsTestModel();
+
+    $fields = $model->sendThroughPipeline($model->fieldsCollection(), [
+            ApplyTabs::class,
+            MapFields::class,
+            AddIdsToFields::class,
+            ApplyParentConditionalLogic::class,
+            ApplyParentDisplayAttributes::class,
+            FilterCreateFields::class,
+    ]);
+
+    // expect count to be 1
+    expect(count($fields))->toBe(3);
+
+    // tab-1 is not in the fields
+    expect($fields->where('slug', 'tab-1')->count())->toBe(0);
+    expect($fields->where('slug', 'text1')->count())->toBe(0);
+    expect($fields->where('slug', 'tab-2')->count())->toBe(1);
+    expect($fields->where('slug', 'text2')->count())->toBe(1);
 });
