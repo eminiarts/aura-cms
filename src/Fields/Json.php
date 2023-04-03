d999ecb0862092b34d077c8ebea97fb69382c950
Bajram Emini <bajram@eminiarts.ch>
2023-04-03 12:09:12 +0200

    Create Json.php

---
+++ src/Fields/Json.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Eminiarts\Aura\Fields;
+
+class Json extends Field
+{
+    public $component = 'aura::fields.code';
+
+    // public $view = 'components.fields.code';
+
+    public function get($field, $value)
+    {
+
+        return $value;
+        return json_decode($value, true);
+    }
+      public function set($value)
+      {
+          if(is_array($value)) {
+              return json_encode($value);
+          }
+
+          return $value;
+      }
+
+    public function getFields()
+    {
+        return array_merge(parent::getFields(), [
+            [
+                'label' => 'Code',
+                'name' => 'Code',
+                'type' => 'Eminiarts\\Aura\\Fields\\Tab',
+                'slug' => 'code',
+                'style' => [],
+            ],
+            [
+                'label' => 'Language',
+                'name' => 'Language',
+                'type' => 'Eminiarts\\Aura\\Fields\\Select',
+                'validation' => 'required',
+                'slug' => 'language',
+                'options' => [
+                    'html' => 'HTML',
+                    'css' => 'CSS',
+                    'javascript' => 'JavaScript',
+                    'php' => 'PHP',
+                    'json' => 'JSON',
+                    'yaml' => 'YAML',
+                    'markdown' => 'Markdown',
+                ],
+            ],
+        ]);
+    }
+
+
+}
