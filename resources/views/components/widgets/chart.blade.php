29201387d369788f8ff83d18fdbbeafd463236a2
Enes Emini <enes.emini@gmail.com>
2023-04-04 15:09:27 +0200

    WIP Chart Widget

---
+++ resources/views/components/widgets/chart.blade.php
@@ -0,0 +1,143 @@
+<div class="aura-card">
+  {{-- @dump($start)
+  @dump($end)
+  @dump($widget)
+  @dump($this->values) --}}
+
+  <div class="p-2">
+    <div class="flex items-baseline justify-between mb-4">
+      <span class="text-sm font-semibold">Chart {{ $widget['name'] }}</span>
+
+      <div class="ml-4">
+        <span class="text-xs font-normal text-gray-500">{{ $start }} - {{ $end }}</span>
+      </div>
+    </div>
+
+    <div class="flex items-baseline justify-between mt-1 mb-2 md:block lg:flex">
+      <div class="flex items-baseline text-4xl font-medium">
+
+        <div>
+          <div
+          x-data="{
+              values: {{ json_encode(array_values($this->values['current'])) }},
+              labels: {{ json_encode(array_keys($this->values['current'])) }},
+
+              init() {
+                  let chart = new ApexCharts(this.$refs.chart, this.options)
+
+                  chart.render()
+
+                  this.$watch('values', () => {
+                      chart.updateOptions(this.options)
+                  })
+
+                  window.dispatchEvent(new Event('resize'));
+              },
+              get options() {
+                  return {
+                      chart: { type: 'area',
+                      toolbar: false,
+
+
+                  },
+
+                  colors:[getCssVariableValue('--primary-400')],
+
+                  stroke: {
+                      curve: 'smooth',
+                  },
+                  fill: {
+                      type: 'gradient',
+                      gradient: {
+                          shadeIntensity: 1,
+                          gradientToColors: ['#B3B3B300'],
+                          opacityFrom: 0.7,
+                          opacityTo: 0.9,
+                          stops: [0, 90, 100]
+                      }
+                  },
+
+                  theme: {
+                      mode: 'light',
+                      palette: 'palette1',
+                      monochrome: {
+                          enabled: false,
+                          color: '#255aee',
+                          shadeTo: 'light  ',
+                          shadeIntensity: 0.65 },
+                      },
+
+                      dataLabels: {
+                          enabled: false,},
+                          tooltip: {
+                              marker: false,
+                          },
+                          xaxis: { categories: this.labels,
+
+                              labels: {show:false,
+                              },
+                          },
+
+
+                          yaxis: {
+                              labels: {show:false,
+                              },
+                          },
+
+
+                          grid: {
+                              show: false,
+                          },
+
+                          series: [{
+                              name: 'Created',
+                              data: this.values,
+                          }],
+
+
+                      }
+                  }
+              }"
+              class="w-full"
+              >
+              <div x-ref="chart" class="bg-white rounded-lg dark:bg-gray-800"></div>
+          </div>
+
+          {{-- @dump($this->values['current'], array_keys($this->values['current']), array_values($this->values['current'])) --}}
+        </div>
+
+
+
+        {{-- {{ $this->values['current'] ?? 'N/A' }} --}}
+      </div>
+
+      {{-- <div>
+        <div class="inline-flex items-baseline rounded-full px-2.5 py-0.5 text-sm font-medium bg-green-100 text-green-800 md:mt-2 lg:mt-0">
+        <svg class="-ml-1 mr-0.5 h-5 w-5 flex-shrink-0 self-center text-green-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+          <path fill-rule="evenodd" d="M10 17a.75.75 0 01-.75-.75V5.612L5.29 9.77a.75.75 0 01-1.08-1.04l5.25-5.5a.75.75 0 011.08 0l5.25 5.5a.75.75 0 11-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0110 17z" clip-rule="evenodd" />
+        </svg>
+        <span class="sr-only"> Increased by </span>
+        {{-- {{ $this->values['change'] }}%
+      </div> <span class="text-sm">for the period</span>
+      </div> --}}
+    </div>
+
+    {{-- <div>
+      <span class="text-sm font-medium text-gray-500">from {{ $this->values['previous'] }}</span>
+    </div> --}}
+
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
+  <script>
+      // after 100ms trigger a window resize event to force the chart to redraw
+      setTimeout(function() {
+          window.dispatchEvent(new Event('resize'));
+      }, 0);
+      setTimeout(function() {
+          window.dispatchEvent(new Event('resize'));
+      }, 100);
+  </script>
+
+</div>
--- src/Widgets/Widgets.php
+++ src/Widgets/Widgets.php
@@ -41,6 +41,30 @@ class Widgets extends Component
         if ($this->selected === 'all') {
             $this->start = null;
             $this->end = null;
+        } elseif ($this->selected === 'ytd') {
+            $this->start = Carbon::now()->startOfYear()->toDateString();
+            $this->end = Carbon::now()->toDateString();
+        } elseif ($this->selected === 'qtd') {
+            $this->start = Carbon::now()->startOfQuarter()->toDateString();
+            $this->end = Carbon::now()->toDateString();
+        } elseif ($this->selected === 'mtd') {
+            $this->start = Carbon::now()->startOfMonth()->toDateString();
+            $this->end = Carbon::now()->toDateString();
+        } elseif ($this->selected === 'wtd') {
+            $this->start = Carbon::now()->startOfWeek()->toDateString();
+            $this->end = Carbon::now()->toDateString();
+        } elseif ($this->selected === 'last-month') {
+            $this->start = Carbon::now()->subMonth()->startOfMonth()->toDateString();
+            $this->end = Carbon::now()->subMonth()->endOfMonth()->toDateString();
+        } elseif ($this->selected === 'last-week') {
+            $this->start = Carbon::now()->subWeek()->startOfWeek()->toDateString();
+            $this->end = Carbon::now()->subWeek()->endOfWeek()->toDateString();
+        } elseif ($this->selected === 'last-quarter') {
+            $this->start = Carbon::now()->subQuarter()->startOfQuarter()->toDateString();
+            $this->end = Carbon::now()->subQuarter()->endOfQuarter()->toDateString();
+        } elseif ($this->selected === 'last-year') {
+            $this->start = Carbon::now()->subYear()->startOfYear()->toDateString();
+            $this->end = Carbon::now()->subYear()->endOfYear()->toDateString();
         } else {
             $interval = intval(preg_replace('/[^0-9]/', '', $this->selected));
             $this->start = Carbon::now()->subDays($interval)->toDateString();
