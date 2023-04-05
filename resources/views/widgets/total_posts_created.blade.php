009aba76970d968d194be575461e671df838e478
Bajram Emini <bajram@eminiarts.ch>
2023-04-05 21:25:20 +0200

    Refactor Value Widget, delete old Widgets

--- resources/views/widgets/total_posts_created.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="aura-card">
-  <div class="total-posts-created-widget">
-    <h3>Total Posts Created: {{ $totalPosts }}</h3>
-    <p>Interval: {{ $interval->forHumans(['parts' => 1]) }}</p>
-  </div>
-</div>
