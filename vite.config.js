7a54e6c35b834561ae3368936984c8af1a69e8c9
Enes Emini <enes.emini@gmail.com>
2023-04-25 14:56:28 +0200

    WIP

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js'
+                'resources/js/app.js',
+                'resources/js/apexcharts.js'
             ],
             refresh: true,
             publicDirectory: 'resources',
