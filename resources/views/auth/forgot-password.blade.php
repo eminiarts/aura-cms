569c4f6e6e63b3b8d219447ed6ef87ae0c26d0e6
Bajram Emini <bajram@eminiarts.ch>
2023-04-25 16:03:17 +0200

    Fixing Forgot Password and Reset Link

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,4 +1,4 @@
-<x-aura::layout.guest>
+<x-aura::layout.login>
     <div class="mb-4 text-sm text-gray-600">
         {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
     </div>
@@ -22,4 +22,4 @@
             </x-aura::primary-button>
         </div>
     </form>
-</x-aura::layout.guest>
+</x-aura::layout.login>
--- src/Http/Controllers/Auth/PasswordResetLinkController.php
+++ src/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -38,6 +38,8 @@ class PasswordResetLinkController extends Controller
             $request->only('email')
         );
 
+        // dd('hier', $status);
+
         return $status == Password::RESET_LINK_SENT
                     ? back()->with('status', __($status))
                     : back()->withInput($request->only('email'))
