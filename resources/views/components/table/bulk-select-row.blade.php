da5b5dc3d6919cbe640d7b0e2ecfc063e236bfe2
Bajram Emini <bajram@eminiarts.ch>
2023-04-29 12:24:11 +0200

    Update bulk-select-row.blade.php

--- resources/views/components/table/bulk-select-row.blade.php
+++ resources/views/components/table/bulk-select-row.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <template x-if="selectPage && !selectAll">
+    <template x-if="selectPage">
         <div class="p-2 bg-primary-50 dark:bg-gray-900 dark:text-white text-gray-900 rounded-lg text-sm">
             <template x-if="!selectAll">
                 <div>
@@ -10,12 +10,12 @@
                     </span>
 
                     <button x-on:click="selectAll=true" type="button"
-                        class="ml-1 text-sm font-medium leading-5 text-gray-700 text-blue-600 underline transition duration-150 ease-in-out focus:outline-none focus:text-gray-800 focus:underline dark:text-white dark:hover:text-gray-400">
+                        class="ml-1 text-sm font-medium leading-5 text-gray-700 text-primary-600 underline transition duration-150 ease-in-out focus:outline-none focus:text-gray-800 focus:underline dark:text-white dark:hover:text-gray-400">
                         Select All
                     </button>
 
                     <button x-on:click="resetBulk();" type="button"
-                        class="ml-1 text-sm font-medium leading-5 text-gray-700 text-blue-600 underline transition duration-150 ease-in-out focus:outline-none focus:text-gray-800 focus:underline dark:text-white dark:hover:text-gray-400">
+                        class="ml-1 text-sm font-medium leading-5 text-gray-700 text-primary-600 underline transition duration-150 ease-in-out focus:outline-none focus:text-gray-800 focus:underline dark:text-white dark:hover:text-gray-400">
                         Unselect All
                     </button>
                 </div>
@@ -29,13 +29,11 @@
                     </span>
 
                     <button x-on:click="resetBulk()" type="button"
-                        class="ml-1 text-sm font-medium leading-5 text-gray-700 text-blue-600 underline transition duration-150 ease-in-out focus:outline-none focus:text-gray-800 focus:underline dark:text-white dark:hover:text-gray-400">
+                        class="ml-1 text-sm font-medium leading-5 text-gray-700 text-primary-600 underline transition duration-150 ease-in-out focus:outline-none focus:text-gray-800 focus:underline dark:text-white dark:hover:text-gray-400">
                         Unselect All
                     </button>
                 </div>
             </template>
         </div>
-
     </template>
-
 </div>
