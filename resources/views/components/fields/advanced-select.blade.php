aeb9144dbe1e54e985b82b190e135b482eb4cea0
Enes Emini <enes.emini@gmail.com>
2023-05-02 21:20:37 +0200

    Fix AdvancedSelect Height

--- resources/views/components/fields/advanced-select.blade.php
+++ resources/views/components/fields/advanced-select.blade.php
@@ -27,7 +27,7 @@
 @endphp
 
 {{-- @dump(optional($field['field'])->api) --}}
-{{-- @dump($create) 
+{{-- @dump($create)
 @dump($field)  --}}
 
 <x-aura::fields.wrapper :field="$field">
@@ -341,7 +341,7 @@
         <label class="sr-only">Select Item</label>
 
         <button
-            class="relative flex items-center justify-between w-full px-3 pt-0 pb-0 border rounded-lg shadow-xs appearance-none min-h-10 border-gray-500/30 focus:border-primary-300 focus:outline-none ring-gray-900/10 focus:ring focus:ring-primary-300 focus:ring-opacity-50 dark:focus:ring-primary-500 dark:focus:ring-opacity-50 dark:bg-gray-900 dark:border-gray-700"
+            class="relative flex items-center justify-between w-full px-3 pt-0 pb-0 border rounded-lg shadow-xs appearance-none min-h-[2.625rem] border-gray-500/30 focus:border-primary-300 focus:outline-none ring-gray-900/10 focus:ring focus:ring-primary-300 focus:ring-opacity-50 dark:focus:ring-primary-500 dark:focus:ring-opacity-50 dark:bg-gray-900 dark:border-gray-700"
             x-ref="listboxButton"
             @click="toggleListbox"
         >
@@ -471,7 +471,7 @@
 
     @if($create)
 <button wire:click.prevent="$emit('openModal', 'aura::post-create-modal', {{ json_encode(['type' => $field['resource'], 'params' => [ 'for' => $field['slug'] ]
-]) }})" class="cursor-pointer text-bold text-sm">
+]) }})" class="text-sm cursor-pointer text-bold">
     + Create
 </button>
 @endif
