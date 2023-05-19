7247d49bc41733616d36fd1699b580cc3c71299e
Bajram Emini <bajram@eminiarts.ch>
2023-05-19 18:35:54 +0200

    WIP Table Selection of Rows

--- resources/views/components/table/bulk-select-row.blade.php
+++ resources/views/components/table/bulk-select-row.blade.php
@@ -1,17 +1,21 @@
-<div>
-    <template x-if="selectPage">
+    <template x-if="selected && selected.length > 0">
+
         <div class="p-2 bg-primary-50 dark:bg-gray-900 dark:text-white text-gray-900 rounded-lg text-sm">
+
             <template x-if="!selectAll">
                 <div>
-                    <span>
-                        You have selected
-                        <strong x-text="selected.length"></strong>
-                        <span x-text="selected.length === 1 ? 'row' : 'rows'"></span>.
-                    </span>
+                    <template x-if="selected">
+                        <span>
+                            You have selected
+                            <strong x-text="selected.length"></strong>
+                            <span x-text="selected.length === 1 ? 'row' : 'rows'"></span>.
+                        </span>
+                    </template>
 
-                    <button x-on:click="selectAll=true" type="button"
+                    <button x-on:click="selectAllRows" type="button"
                         class="ml-1 text-sm font-medium leading-5 text-gray-700 text-primary-600 underline transition duration-150 ease-in-out focus:outline-none focus:text-gray-800 focus:underline dark:text-white dark:hover:text-gray-400">
-                        Select All
+                        Select all <strong x-text="total"></strong>
+                        rows.
                     </button>
 
                     <button x-on:click="resetBulk();" type="button"
@@ -20,6 +24,7 @@
                     </button>
                 </div>
             </template>
+
             <template x-if="selectAll">
                 <div>
                     <span>
@@ -36,4 +41,3 @@
             </template>
         </div>
     </template>
-</div>
--- resources/views/components/table/index.blade.php
+++ resources/views/components/table/index.blade.php
@@ -5,66 +5,85 @@
 ])
 
 <div class="mx-auto max-w-8xl" wire:key="table-index-{{ str()->random(4) }}" x-data="{
-                selected: @entangle('selected').defer,
-                rows: @js($this->rows->pluck('id')->toArray()), 
-                lastSelectedId: null,
-                total: @js($this->rows->total()),
-                selectPage: false,
-                selectAll: @entangle('selectAll'),
-
-                init() {
-                      Livewire.on('selectedRows', (updatedSelected) => {
-                            this.selected = updatedSelected;
-                        });
-                },
-                selectAllRows() {
-                    this.$nextTick(() => {
-                        if (this.selectPage) {
-                            this.selected = this.rows.slice();
-                        } else {
-                            this.selected = [];
-                        }
-                    });
-                },
-                resetBulk() {
-                    this.selected = [];
-                    this.selectPage = false;
-                    this.selectAll = false;
-                },
-                toggleRow(event, id) {
-                    this.$nextTick(() => {
-                        // Check if shift key was pressed and last selected id exists
-                        if (event.shiftKey && this.lastSelectedId !== null) {
-                            // Get the indexes of the current and last selected rows
-                            const lastIndex = this.rows.indexOf(this.lastSelectedId);
-                            const currentIndex = this.rows.indexOf(id);
-
-                            // Determine the start and end indexes of the rows to be selected
-                            const start = Math.min(lastIndex, currentIndex);
-                            const end = Math.max(lastIndex, currentIndex);
-
-                            // If the current row is not already selected, remove all rows between start and end
-                            if (!this.selected.includes(id.toString())) {
-                                this.selected = this.selected.filter(row => !this.rows.slice(start, end + 1).map(item => item.toString()).includes(row.toString()));
-                            }
-                            // Otherwise, add all rows between start and end
-                            else {
-                                this.selected = [...this.selected, ...this.rows.slice(start, end + 1)].map(item => item.toString());
-
-                                // Remove duplicates from the selected rows
-                                this.selected = this.selected.filter((item, index) => this.selected.indexOf(item) === index);
-                            }
-                        }
-
-                        this.lastSelectedId = id;
-                    });
-                }
-            }">
-
-    <div>
-        @include('aura::components.table.bulk-select-row')
-    </div>
-
-    @include($this->model->tableView())
+    selected: @entangle('selected').defer,
+    rows: @js($this->rows->pluck('id')->toArray()), 
+    lastSelectedId: null,
+    total: @js($this->rows->total()),
+    selectPage: false,
+    selectAll: @entangle('selectAll'),
+    loading: false,
     
+    init() {
+        Livewire.on('selectedRows', (updatedSelected) => {
+            this.selected = updatedSelected;
+        });
+        
+        if(this.selectAll) {
+            this.selectPage = true;
+        }
+    },
+
+    selectCurrentPage() {
+         this.$nextTick(() => {
+                if (this.selectPage) {
+                    this.selected = this.rows.slice();
+                } else {
+                    this.selected = [];
+                }
+            });
+    },
+
+    selectAllRows: async function () {
+
+            this.loading = true
+
+            this.selected = await $wire.getAllTableRows()
+
+            console.log(this.selected, 'selected')
+
+            this.loading = false
+        },
+   
+    resetBulk() {
+        this.selected = [];
+        this.selectPage = false;
+        this.selectAll = false;
+    },
+    
+    toggleRow(event, id) {
+        this.$nextTick(() => {
+            // Check if shift key was pressed and last selected id exists
+            if (event.shiftKey && this.lastSelectedId !== null) {
+                // Get the indexes of the current and last selected rows
+                const lastIndex = this.rows.indexOf(this.lastSelectedId);
+                const currentIndex = this.rows.indexOf(id);
+                
+                // Determine the start and end indexes of the rows to be selected
+                const start = Math.min(lastIndex, currentIndex);
+                const end = Math.max(lastIndex, currentIndex);
+                
+                // If the current row is not already selected, remove all rows between start and end
+                if (!this.selected.includes(id.toString())) {
+                    this.selected = this.selected.filter(row => !this.rows.slice(start, end + 1).map(item => item.toString()).includes(row.toString()));
+                }
+                // Otherwise, add all rows between start and end
+                else {
+                    this.selected = [...this.selected, ...this.rows.slice(start, end + 1)].map(item => item.toString());
+                    
+                    // Remove duplicates from the selected rows
+                    this.selected = this.selected.filter((item, index) => this.selected.indexOf(item) === index);
+                }
+            }
+            
+            this.lastSelectedId = id;
+        });
+    }
+}">
+
+<div>
+    @include('aura::components.table.bulk-select-row')
+</div>
+
+@include($this->model->tableView())
+
 </div>
