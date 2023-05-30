16b7f780d919717243ca4e48c5a3b2c23e0d83a3
Bajram Emini <bajram@eminiarts.ch>
2023-05-30 23:35:11 +0200

    WIP Search

--- src/Http/Livewire/Table/Traits/Search.php
+++ src/Http/Livewire/Table/Traits/Search.php
@@ -19,11 +19,40 @@ trait Search
     {
 
         if ($this->search) {
-            $query->where(function ($query) {
-                foreach ($this->model->searchableColumns() as $column) {
-                    $query->orWhere($column, 'like', '%'.$this->search.'%');
-                }
+
+            $searchableFields = $this->model->getSearchableFields()->pluck('slug');
+
+
+            $metaFields = $searchableFields->filter(function ($field) {
+                return $this->model->isMetaField($field);
             });
+
+
+
+            if($metaFields->count() > 0) {
+                $query
+                ->select('posts.*', 'post_meta.id as post_meta_id', 'post_meta.post_id', 'post_meta.key', 'post_meta.value')
+                ->leftJoin('post_meta', function ($join) use ($metaFields) {
+                    $join->on('posts.id', '=', 'post_meta.post_id')
+                        ->whereIn('post_meta.key', $metaFields);
+                })->where(function ($query) {
+                    $query->where('posts.title', 'like', '%'.$this->search.'%')
+                        ->orWhere(function ($query) {
+                            $query->where('post_meta.value', 'LIKE', '%'.$this->search.'%');
+                        });
+                })
+                           ->distinct()
+                           //->orderBy('posts.id', 'desc')
+                ;
+            }
+
+            // dd($searchableFields, $metaFields, $query);
+
+            // $query->where(function ($query) {
+            //     foreach ($this->model->searchableColumns() as $column) {
+            //         $query->orWhere($column, 'like', '%'.$this->search.'%');
+            //     }
+            // });
         }
 
         return $query;
