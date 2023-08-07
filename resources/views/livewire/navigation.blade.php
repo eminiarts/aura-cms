a815371e7634295ab9c0be9dd6b656c87ae8d16c
Bajram Emini <bajram@eminiarts.ch>
2023-08-07 23:14:51 +0200

    Fix Navigation

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -94,16 +94,12 @@ $appSettings = Aura::options();
 
     <div class="flex-shrink-0 px-5 h-[4.5rem] flex items-center border-t border-white border-opacity-20 dark:border-gray-700">
         <x-aura::tippy-area text="{{ Auth::user()->name }}" position="top">
-            <x-slot:title>
-            <img class="inline-block rounded-full h-9 w-9" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-            </x-slot:title>
-
-            
+                <x-slot:title>
+                <img class="inline-block rounded-full h-9 w-9" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                </x-slot:title>
+                @include('aura::navigation.footer-popup')
         </x-aura::tippy-area>
 
-        <div>
-            @include('aura::navigation.footer-popup')
-        </div>
     </div>
   </div>
 </div>
@@ -246,9 +242,11 @@ $appSettings = Aura::options();
                     </div>
                 </x-slot:title>
 
+                @include('aura::navigation.footer-popup')
+
             </x-aura::navigation.team-switcher>
 
-            {{-- @include('aura::navigation.footer-popup') --}}
+            
 
             <div class="ml-2">
                 <x-aura::tippy text="Notifications">
