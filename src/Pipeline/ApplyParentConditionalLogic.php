dcb90bdc08a18caf4c5ea89853151eabfbe75f1c
Bajram Emini <bajram@eminiarts.ch>
2023-10-11 16:55:05 +0200

    Improving Pipeline

--- src/Pipeline/ApplyParentConditionalLogic.php
+++ src/Pipeline/ApplyParentConditionalLogic.php
@@ -6,35 +6,44 @@ use Closure;
 
 class ApplyParentConditionalLogic implements Pipe
 {
-    public function getParentIds($fields, $id): array
-    {
-        $parentIds = [];
-
-        // Find the field with the given id
-        $field = $fields->firstWhere('_id', $id);
-
-        // If the field has a parent id, add it to the array and recursively
-        // get the parent ids of the parent field
-        if ($field['_parent_id'] !== null) {
-            $parentIds[] = $field['_parent_id'];
-            $parentIds = array_merge($parentIds, $this->getParentIds($fields, $field['_parent_id']));
-        }
-
-        return $parentIds;
-    }
-
     public function handle($fields, Closure $next)
     {
-        // Foreach $fields as $field, get all parent IDs
-        $fields = $fields->map(function ($field) use ($fields) {
-            $parentIds = $this->getParentIds($fields, $field['_id']);
+        // Build a map of field IDs to parent IDs
+        $parentIdMap = $fields->pluck('_parent_id', '_id');
+
+        // Build a map of field IDs to conditional logics
+        $conditionalLogicMap = $fields->pluck('conditional_logic', '_id');
+
+        // Process fields
+        $fields = $fields->map(function ($field) use ($parentIdMap, $conditionalLogicMap) {
+            $parentIds = $this->getParentIds($parentIdMap, $field['_id']);
 
             // Merge Conditional Logic of parent IDs with current conditional Logic
-            $field['conditional_logic'] = $fields->whereIn('_id', $parentIds)->pluck('conditional_logic')->flatten(1)->filter()->merge(optional($field)['conditional_logic'])->toArray();
+            $parentConditionalLogics = collect($parentIds)
+                ->map(fn ($id) => $conditionalLogicMap[$id] ?? [])
+                ->filter()
+                ->flatten(1);
+
+            $field['conditional_logic'] = $parentConditionalLogics
+                ->merge($field['conditional_logic'] ?? [])
+                ->toArray();
 
             return $field;
         });
 
         return $next($fields);
+    }
+
+    public function getParentIds($parentIdMap, $id): array
+    {
+        $parentIds = [];
+        $currentId = $id;
+
+        while (isset($parentIdMap[$currentId])) {
+            $currentId = $parentIdMap[$currentId];
+            $parentIds[] = $currentId;
+        }
+
+        return $parentIds;
     }
 }
