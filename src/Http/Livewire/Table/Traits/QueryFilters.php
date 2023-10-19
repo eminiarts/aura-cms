5d2493a821011852c29e6bdc8d8c853278a1a99d
Bajram Emini <bajram@eminiarts.ch>
2023-10-19 14:55:10 +0200

    Update QueryFilters.php

--- src/Http/Livewire/Table/Traits/QueryFilters.php
+++ src/Http/Livewire/Table/Traits/QueryFilters.php
@@ -23,7 +23,9 @@ trait QueryFilters
                 continue;
             }
 
-            if ($this->model->isTableField($filter['name'])) {
+            if($this->model->usesCustomTable()) {
+                $query = $this->applyTableFieldFilter($query, $filter);
+            } else if ($this->model->isTableField($filter['name'])) {
                 $query = $this->applyTableFieldFilter($query, $filter);
             } else {
                 $query = $this->applyMetaFieldFilter($query, $filter);
@@ -32,7 +34,7 @@ trait QueryFilters
 
         // More advanced Search
         if ($this->search) {
-            $query->where('title', 'LIKE', $this->search.'%');
+         //   $query->where($this->model->getTable() . '.title', 'LIKE', $this->search.'%');
         }
 
         return $query;
