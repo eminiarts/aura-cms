c2a39abb6849b6d061a10ee2402512de651969de
Bajram Emini <bajram@eminiarts.ch>
2023-10-25 23:19:41 +0200

    WIP ConditionalLogic

--- resources/views/components/fields/fields.blade.php
+++ resources/views/components/fields/fields.blade.php
@@ -1,10 +1,10 @@
-@aware(['field', 'model'])
+@aware(['field', 'model', 'post'])
 
 <div class="flex flex-wrap items-start -mx-2" wire:key="tab-field-{{ md5(json_encode($field)) }}">
     @if(optional($field)['fields'])
         @foreach($field['fields'] as $key => $field)
         {{-- @dump($key) --}}
-            @checkCondition($this->model ?? $model, $field, $this->post)
+            @checkCondition($this->model ?? $model, $field, $this->post ?? $post)
                 <x-dynamic-component :component="$field['field']->component()" :field="$field" />
             @endcheckCondition
         @endforeach
