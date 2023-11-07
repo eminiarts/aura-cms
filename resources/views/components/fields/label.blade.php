9046c1ce8ed0b294a8d0a52a57e3189217dd73a5
Enes Emini <enes.emini@gmail.com>
2023-11-07 23:08:25 +0100

    Update label.blade.php

--- resources/views/components/fields/label.blade.php
+++ resources/views/components/fields/label.blade.php
@@ -1,3 +1,6 @@
-@props(['label'])
-
+@php
+  ray($label);
+  $label = is_array($label) ? 'Array' : $label;
+  $label = $label ?? 'Label';
+@endphp
 <label class="inline-block mt-3 mb-2 text-sm font-semibold text-gray-800 dark:text-gray-200">{{ $label }}</label>
