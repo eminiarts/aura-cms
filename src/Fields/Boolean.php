f37770a8ff66d03ff9a0b34f39628123d338b942
Bajram Emini <bajram@eminiarts.ch>
2023-11-12 19:30:37 +0100

    🔧 fix(table.blade.php): remove unnecessary console.log statement 🔧 fix(table.blade.php): refactor selection-changed event to include slug only if $field is present 🔧 fix(Boolean.php): refactor display method to return appropriate SVG icons based on boolean value 🔧 fix(Table.php): add selectFieldRows method to handle selecting rows based on field slug 🔧 fix(Table.php): remove ray debug statements

--- src/Fields/Boolean.php
+++ src/Fields/Boolean.php
@@ -28,6 +28,15 @@ class Boolean extends Field
         ]);
     }
 
+    public function display($field, $value, $model)
+    {
+        if ($value) {
+            return '<svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>'; // Check icon from Heroicons
+        } else {
+            return '<svg class="w-6 h-6 text-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>'; // X icon from Heroicons
+        }
+    }
+
     public function get($field, $value)
     {
         return (bool) $value;
