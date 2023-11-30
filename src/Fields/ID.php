0c6760767498ff9dad90577551a79b290c928236
Bajram Emini <bajram@eminiarts.ch>
2023-11-30 14:23:57 +0100

    Create ID.php

---
+++ src/Fields/ID.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Eminiarts\Aura\Fields;
+
+class ID extends Field
+{
+    public $component = 'aura::fields.text';
+
+    public $view = 'aura::fields.view-value';
+
+    public string $type = 'input';
+
+    public $on_forms = false;
+
+
+
+
+
+
+}
