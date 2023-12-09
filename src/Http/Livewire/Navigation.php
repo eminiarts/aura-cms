e9e66ef85729e1c9ea72c5821d818bb08d8774b8
Bajram Emini <bajram@eminiarts.ch>
2023-12-09 18:11:53 +0100

    Pint

--- src/Http/Livewire/Navigation.php
+++ src/Http/Livewire/Navigation.php
@@ -19,7 +19,7 @@ class Navigation extends Component
 
         if (auth()->check() && auth()->user()->getOptionSidebar()) {
             $this->toggledGroups = auth()->user()->getOptionSidebar();
-            // ray($this->toggledGroups);
+        // ray($this->toggledGroups);
         } else {
             $this->toggledGroups = [];
         }
