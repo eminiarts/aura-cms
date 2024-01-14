ad4f59646a96cb74c144bd119e7567a6fd97a141
Enes Emini <enes.emini@gmail.com>
2024-01-15 00:19:55 +0100

    WIP

--- src/Http/Livewire/Post/Create.php
+++ src/Http/Livewire/Post/Create.php
@@ -105,7 +105,7 @@ class Create extends Component
 
     public function save()
     {
-        // dd($this->rules());
+        // dd($this->model->toArray(), $this->rules());
 
         $this->validate();
 
@@ -150,5 +150,41 @@ class Create extends Component
                 $this->post['fields'][$slug] = $field['default'];
             }
         }
+    }
+
+    public function callMethod($method, $params = [], $captureReturnValueCallback = null)
+    {
+        // If the method exists in this component, call it directly.
+        if (method_exists($this, $method) || ! optional($params)[0]) {
+            return parent::callMethod($method, $params, $captureReturnValueCallback);
+        }
+
+        // Assuming the first parameter is always the slug to identify the field.
+        $slug = $params[0];
+
+        // Get the corresponding field instance based on the slug.
+        $field = $this->model->fieldBySlug($slug);
+
+        // Forward the call to the field's method.
+        if ($field) {
+
+            $fieldTypeInstance = app($field['type']);
+
+            // If the method exists in the field type, call it directly.
+            if (method_exists($fieldTypeInstance, $method)) {
+                $post = call_user_func_array([$fieldTypeInstance, $method], array_merge([$this->model, $this->post], $params));
+
+                // If the field type method returns a post, update the post.
+                if ($post) {
+                    $this->post = $post;
+                }
+
+                // Make sure to return here, otherwise the parent callMethod will be called.
+                return;
+            }
+        }
+
+        // Run parent callMethod
+        return parent::callMethod($method, $params, $captureReturnValueCallback);
     }
 }
