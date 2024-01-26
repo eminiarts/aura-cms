6eeb86b209a9f6b50979ed4aef148fc421cdd86f
Enes Emini <enes.emini@gmail.com>
2024-01-26 17:20:28 +0100

    Fix Verify Email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -4,13 +4,13 @@
     </div>
 
     @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600">
+        <div class="mb-4 text-sm font-medium text-green-600">
             {{ __('A new verification link has been sent to the email address you provided during registration.') }}
         </div>
     @endif
 
-    <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('aura.verification.send') }}">
+    <div class="flex justify-between items-center mt-4">
+        <form method="POST" action="{{ route('verification.send') }}">
             @csrf
 
             <div>
@@ -23,7 +23,7 @@
         <form method="POST" action="{{ route('logout') }}">
             @csrf
 
-            <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
+            <button type="submit" class="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
                 {{ __('Log Out') }}
             </button>
         </form>
