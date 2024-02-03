da1f8ab43b261c9622d42ec41311dcc026a9e9cf
Bajram Emini <bajram@eminiarts.ch>
2024-02-03 03:42:48 +0100

    WIP

--- resources/views/components/button/light.blade.php
+++ resources/views/components/button/light.blade.php
@@ -1,8 +1,10 @@
 @props([
   'size' => 'base',
-  'class' => ''
+  'class' => '',
+  'navigate' => true
 ])
 <x-aura::button
+  :navigate="$navigate"
   size="{{ $size }}"
   {{$attributes->merge([
     'class' => $class . ' text-primary-600 bg-primary-100/60 dark:bg-primary-100 transition-all border border-transparent hover:bg-primary-200 focus:ring-primary-300 dark:focus:ring-primary-500 shadow-none',
