d05cbd88bcb45918352fcadfde50aeadc97a928c
Bajram Emini <bajram@eminiarts.ch>
2024-02-08 14:14:52 +0100

    Fixing Tests

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -14,7 +14,7 @@ test('login screen can be rendered', function () {
 test('users can authenticate using the login screen', function () {
     $user = User::factory()->create();
 
-    $response = $this->resource('/login', [
+    $response = $this->post('/login', [
         'email' => $user->email,
         'password' => 'password',
     ]);
@@ -26,7 +26,7 @@ test('users can authenticate using the login screen', function () {
 test('users can not authenticate with invalid password', function () {
     $user = User::factory()->create();
 
-    $this->resource('/login', [
+    $this->post('/login', [
         'email' => $user->email,
         'password' => 'wrong-password',
     ]);
