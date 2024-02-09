32d9d441b8251e0b4b5e0a525283e6773f30949d
Bajram Emini <bajram@eminiarts.ch>
2024-02-09 21:17:20 +0100

    WIP

--- tests/Feature/TableIndexTest.php
+++ tests/Feature/TableIndexTest.php
@@ -6,8 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 beforeEach(fn () => $this->actingAs($this->user = createSuperAdmin()));
 
-
-
 test('post_meta table index test', function () {
     $columns = Schema::getColumnListing('post_meta');
     expect($columns)->toContain('post_id');
@@ -16,11 +14,22 @@ test('post_meta table index test', function () {
 
     $indexes = Schema::getConnection()->getDoctrineSchemaManager()->listTableIndexes('post_meta');
 
-    expect($indexes)->toHaveCount(3);
+    expect($indexes)->toHaveCount(4);
 
     $this->assertTrue($indexes['primary']->isPrimary());
     expect($indexes['primary']->getColumns())->toBe(['id']);
 
     expect($indexes['post_meta_post_id_index'])->not->toBeNull();
     expect($indexes['post_meta_key_index'])->not->toBeNull();
+});
+
+
+test('posts table index test', function () {
+    $columns = Schema::getColumnListing('posts');
+
+    $indexes = Schema::getConnection()->getDoctrineSchemaManager()->listTableIndexes('posts');
+
+    // dump($indexes);
+
+    expect($indexes)->toHaveCount(7);
 });
