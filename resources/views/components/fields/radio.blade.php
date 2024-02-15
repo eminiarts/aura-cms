e7ee2dbca05849607a358ff5d3c29a6f57f5f010
Enes Emini <enes.emini@gmail.com>
2024-02-15 23:59:03 +0100

    WIP Fields

--- resources/views/components/fields/radio.blade.php
+++ resources/views/components/fields/radio.blade.php
@@ -1,7 +1,9 @@
 <x-aura::fields.wrapper :field="$field">
-
-    @foreach($field['options'] as $key => $option)
-        <x-aura::input.radio wire:model="form.fields.{{ optional($field)['slug'] }}" name="post_fields_{{ optional($field)['slug'] }}" id="post_fields_{{ optional($field)['slug'] }}" :label="$option" :value="$key" />
+@php
+ray($field);
+@endphp
+    @foreach(optional($field)['options'] as $key => $option)
+        <x-aura::input.radio wire:model="form.fields.{{ optional($field)['slug'] }}" name="post_fields_{{ optional($field)['slug'] }}" id="post_fields_{{ optional($field)['slug'] }}" :label="$option['name']" :value="$option['value']" />
     @endforeach
 
 </x-aura::fields.wrapper>
