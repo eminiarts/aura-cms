4cb2497abcd8a4deb543093cf34ccc9d9a4b3b6b
Bajram Emini <bajram@eminiarts.ch>
2024-02-16 13:33:49 +0100

    Update radio.blade.php

--- resources/views/components/fields/radio.blade.php
+++ resources/views/components/fields/radio.blade.php
@@ -2,8 +2,10 @@
 @php
 ray($field);
 @endphp
+
+
     @foreach(optional($field)['options'] as $key => $option)
-        <x-aura::input.radio wire:model="form.fields.{{ optional($field)['slug'] }}" name="post_fields_{{ optional($field)['slug'] }}" id="post_fields_{{ optional($field)['slug'] }}" :label="$option['name']" :value="$option['value']" />
+        <x-aura::input.radio wire:model="form.fields.{{ optional($field)['slug'] }}" name="post_fields_{{ optional($field)['slug'] }}" id="post_fields_{{ optional($field)['slug'] }}" :label="$option" :value="$key" />
     @endforeach
 
 </x-aura::fields.wrapper>
