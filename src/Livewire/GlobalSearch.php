19577d2f21cdfffb9a2ed6baf58cb5d46379986f
Enes Emini <enes.emini@gmail.com>
2024-03-18 12:30:10 +0100

    WIP GlobalSearch Fixes

--- src/Livewire/GlobalSearch.php
+++ src/Livewire/GlobalSearch.php
@@ -23,9 +23,15 @@ class GlobalSearch extends Component
 
         // Initialize search results array
 
+        ray($resources);
+
         // filter out flows and flow_logs from resources
         $resources = array_filter($resources, function ($resource) {
-            // dump($resource::getSlug());
+            if ($resource === null) {
+                return false;
+            }
+            ray($resource);
+            // ray($resource::getSlug());
             return $resource::getSlug() !== 'resource' && $resource::getSlug() !== 'flow' && $resource::getSlug() !== 'flowlog' && $resource::getSlug() !== 'operation' && $resource::getSlug() !== 'flowoperation' && $resource::getSlug() !== 'operationlog' && $resource::getSlug() !== 'option' && $resource::getSlug() !== 'team' && $resource::getSlug() !== 'user' && $resource::getSlug() !== 'product';
         });
 
