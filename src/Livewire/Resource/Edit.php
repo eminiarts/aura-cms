68ca5781dbdc4e5912fa3647439c59b08c01210a
Bajram Emini <bajram@eminiarts.ch>
2024-04-10 22:22:14 +0200

    WIP

--- src/Livewire/Resource/Edit.php
+++ src/Livewire/Resource/Edit.php
@@ -21,22 +21,22 @@ class Edit extends Component
     use InteractsWithFields;
     use MediaFields;
     use RepeaterFields;
-
+    
     // use Macroable;
     use WithFileUploads;
-
+    
     public $form;
-
+    
     public $inModal = false;
-
+    
     public $model;
-
+    
     public $slug;
-
+    
     public $tab;
-
+    
     public $tax;
-
+    
     // Listen for selectedAttachment
     protected $listeners = [
         'updateField' => 'updateField',
@@ -45,43 +45,30 @@ class Edit extends Component
         'reload',
         'saveBeforeAction',
     ];
-
-    public function callMethod($method, $params = [], $captureReturnValueCallback = null)
+    
+    
+    
+    protected function callComponentMethod($method, $params)
     {
-        // If the method exists in this component, call it directly.
-        if (method_exists($this, $method) || ! optional($params)[0]) {
-            return parent::callMethod($method, $params, $captureReturnValueCallback);
+        $callbacks = array_filter(
+            $params,
+            fn($param) => $param instanceof Closure
+        );
+        
+        $params = array_filter(
+            $params,
+            fn($param) => ! $param instanceof Closure
+        );
+        
+        $result = parent::callMethod($method, $params);
+        
+        foreach ($callbacks as $callback) {
+            $callback($this);
         }
-
-        // Assuming the first parameter is always the slug to identify the field.
-        $slug = $params[0];
-
-        // Get the corresponding field instance based on the slug.
-        $field = $this->model->fieldBySlug($slug);
-
-        // Forward the call to the field's method.
-        if ($field) {
-
-            $fieldTypeInstance = app($field['type']);
-
-            // If the method exists in the field type, call it directly.
-            if (method_exists($fieldTypeInstance, $method)) {
-                $post = call_user_func_array([$fieldTypeInstance, $method], array_merge([$this->model, $this->form], $params));
-
-                // If the field type method returns a post, update the post.
-                if ($post) {
-                    $this->form = $post;
-                }
-
-                // Make sure to return here, otherwise the parent callMethod will be called.
-                return;
-            }
-        }
-
-        // Run parent callMethod
-        return parent::callMethod($method, $params, $captureReturnValueCallback);
+        
+        return $result;
     }
-
+    
     public function initializeModelFields()
     {
         foreach ($this->model->inputFields() as $field) {
@@ -89,43 +76,43 @@ class Edit extends Component
             if (method_exists($field['field'], 'hydrate')) {
                 $this->form['fields'][$field['slug']] = $field['field']->hydrate();
             }
-
+            
             if($field['field']->on_forms === false) {
                 unset($this->form['fields'][$field['slug']]);
             }
-
+            
             if(optional($field)['on_forms'] === false) {
                 unset($this->form['fields'][$field['slug']]);
             }
         }
     }
-
+    
     public function mount($slug, $id)
     {
         $this->slug = $slug;
-
+        
         $this->model = Aura::findResourceBySlug($slug)->find($id);
-
+        
         // Authorize
         $this->authorize('update', $this->model);
-
+        
         // Array instead of Eloquent Model
         $this->form = $this->model->attributesToArray();
         
-
+        
         // dd($this->model->attributesToArray());
-
+        
         // foreach fields, call the hydration method on the field
         $this->initializeModelFields();
-
+        
         // foreach fields, call the hydration method on the field
-
+        
         // ray('mount', $this->form, $this->model);
-
+        
         // Set on model instead of here
         // if $this->form['terms']['tag'] is not set, set it to null
     }
-
+    
     public function reload()
     {
         $this->model = $this->model->fresh();
@@ -135,58 +122,58 @@ class Edit extends Component
         // Instead, we will refresh the component.
         $this->dispatch('refreshComponent');
     }
-
+    
     public function render()
     {
         return view($this->model->editView())->layout('aura::components.layout.app');
     }
-
+    
     public function rules()
     {
         return collect($this->model->validationRules())->mapWithKeys(function ($rule, $key) {
             return ["form.fields.$key" => $rule];
         })->toArray();
     }
-
+    
     public function save()
     {
         $this->validate();
-
+        
         //  ray()->clearScreen();
         //    dd('saving', $this->form, $this->model);
         //    ray('saving', $this->form, $this->model);
-
+        
         unset($this->form['fields']['group']);
-
+        
         // unset this post fields group
         if ($this->model->usesCustomTable()) {
             $this->model->update($this->form['fields']);
         } else {
             $this->model->update($this->form);
         }
-
+        
         $this->notify(__('Successfully updated'));
-
+        
         if ($this->inModal) {
             $this->dispatch('closeModal');
             $this->dispatch('refreshTable');
         }
-
+        
         $this->model = $this->model->refresh();
         $this->form = $this->model->attributesToArray();
-
+        
         $this->dispatch('refreshComponent');
     }
-
+    
     public function saveBeforeAction($method)
     {
         // Call the save method
         $this->save();
-
+        
         // Emit the 'savedForAction' event with the $method parameter
         $this->dispatch('savedForAction', $method);
     }
-
+    
     public function updatedPost($value, $array)
     {
         // dd('updatedPostFields', $value, $array, $this->form);
