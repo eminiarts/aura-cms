925dd96fd2e547d0242d64ed9e44de1103548f3f
Enes Emini <enes.emini@gmail.com>
2024-05-29 09:35:26 +0200

    Update forgot password dark styles

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
+<label {{ $attributes->merge(['class' => 'inline-block mt-3 mb-2 text-sm font-semibold text-gray-800 dark:text-gray-200']) }}>
     {{ $value ?? $slot }}
 </label>
