bce40627cf101c171ce07430cf9c9dd40e89150b
Bajram Emini <bajram@eminiarts.ch>
2024-06-06 16:22:19 +0200

    Update TeamSettingsTest.php

--- tests/Feature/TeamSettingsTest.php
+++ tests/Feature/TeamSettingsTest.php
@@ -13,7 +13,7 @@ use Livewire\Livewire;
 
 uses(RefreshDatabase::class);
 
-beforeEach(fn () => $this->actingAs($this->user = User::factory()->create()));
+beforeEach(fn () => $this->actingAs($this->user = createSuperAdmin()));
 
 test('Team Settings Component can be rendered', function () {
     $role = Role::create(['type' => 'Role', 'title' => 'Super Admin', 'slug' => 'super_admin', 'description' => 'Super Admin has can perform everything.', 'super_admin' => true, 'permissions' => []]);
@@ -52,13 +52,14 @@ test('Default Team Settings are created', function () {
     $option = Option::first();
 
     // assert option is team settings
-    $this->assertEquals($option->name, 'settings');
+    // $this->assertEquals($option->name, 'settings');
+    $this->assertEquals($option->name, 'team.' . auth()->user()->currentTeam->id . '.settings');
 
     // assert $option->value is an array
     $this->assertIsArray($option->value);
 
-    // assertJSON option value darkmode-type is auto
-    $this->assertJsonStringEqualsJsonString(json_encode($option->value), '{"darkmode-type":"auto","sidebar-type":"primary","color-palette":"aura","gray-color-palette":"slate"}');
+    // assertJSON option value matches expected JSON structure
+    $this->assertJsonStringEqualsJsonString(json_encode($option->value), '{"darkmode-type":"auto","sidebar-type":"primary","color-palette":"aura","gray-color-palette":"slate","sidebar-size":"standard","sidebar-darkmode-type":"dark"}');
 });
 
 test('Team Settings can be saved', function () {
@@ -96,8 +97,7 @@ test('Team Settings can be saved', function () {
     $option = Option::first();
 
     // assert option is team settings
-    $this->assertEquals($option->name, 'settings');
-
+    $this->assertEquals($option->name, 'team.' . auth()->user()->currentTeam->id . '.settings');
     $this->assertIsArray($option->value);
 
     $this->assertEquals('light', $option->value['darkmode-type']);
@@ -107,7 +107,7 @@ test('Team Settings can be saved', function () {
 
     // acting as $this->user, get settings page and assertSee "--primary-400: #f87171;" in html
     $this->actingAs($this->user)
-        ->get(route('aura.team.settings'))
+        ->get(route('aura.settings'))
         ->assertSee('--primary-400: 248 113 113;');
 
     // Default Team Settings
@@ -116,7 +116,7 @@ test('Team Settings can be saved', function () {
         ->call('save');
 
     $this->actingAs($this->user)
-        ->get(route('aura.team.settings'))
+        ->get(route('aura.settings'))
         ->assertDontSee('--primary-400: 248 113 113;')
         ->assertSee('--primary-400: 60 126 244;');
 
@@ -127,7 +127,7 @@ test('Team Settings can be saved', function () {
     $options = Option::get();
 
     $this->actingAs($this->user)
-        ->get(route('aura.team.settings'))
+        ->get(route('aura.settings'))
         ->assertSee('--primary-400: 60 126 244;');
 
     // assert DB has 1 record in options table
@@ -137,7 +137,7 @@ test('Team Settings can be saved', function () {
     $this->user->switchTeam($firstTeam);
 
     $this->actingAs($this->user)
-        ->get(route('aura.team.settings'))
+        ->get(route('aura.settings'))
         ->assertOk()
         ->assertSee('--primary-400: 60 126 244;')
         ->assertDontSee('--primary-400: 248 113 113;')
