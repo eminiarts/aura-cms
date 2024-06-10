f7a594c5905314e59e3195ab68d53c847c16b8ba
Bajram Emini <bajram@eminiarts.ch>
2024-06-11 00:45:07 +0200

    Modal improvements

--- resources/views/livewire/modals.blade.php
+++ resources/views/livewire/modals.blade.php
@@ -1,6 +1,6 @@
 <div>
     @if ($activeModal)
-        <x-aura::dialog wire:model="activeModal">
+        <x-aura::dialog wire:model="activeModal" >
             <x-aura::dialog.panel>
                 @forelse($modals as $id => $modal)
                     @if ($activeModal == $id)
