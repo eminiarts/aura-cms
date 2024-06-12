21ec8a6503ca300eb4071f382eba21019aec7518
Bajram Emini <bajram@eminiarts.ch>
2024-06-12 22:10:17 +0200

    WIP

--- resources/views/components/dialog/panel.blade.php
+++ resources/views/components/dialog/panel.blade.php
@@ -3,8 +3,7 @@
         <div x-dialog x-model="dialogOpen" style="display: none"
             class="fixed inset-0 overflow-y-auto z-10 text-left pt-[30%] sm:pt-0">
             <!-- Overlay -->
-            <div x-dialog:overlay x-transition:enter.opacity class="fixed inset-0 bg-black/25"
-                x-on:click="$dialog.close()"></div>
+            <div x-dialog:overlay x-transition:enter.opacity class="fixed inset-0 bg-black/25"></div>
 
             <!-- Panel -->
             <div x-on:click="$dialog.close()" class="relative min-h-full flex justify-center items-end sm:items-center p-0 sm:p-4">
@@ -26,9 +25,10 @@
                         <div x-on:click="$dialog.close()"
                             class="cursor-pointer bg-gray-50 rounded-lg p-2 text-gray-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2">
                             <span class="sr-only">Close modal</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20"
+                                fill="currentColor">
                                 <path fill-rule="evenodd"
-                                    d="M4.293 4.293a1 1 011.414 0L10 8.586l4.293-4.293a1 1 111.414 1.414L11.414 10l4.293 4.293a1 1 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                                     clip-rule="evenodd" />
                             </svg>
                         </div>
