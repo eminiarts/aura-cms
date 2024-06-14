1354602a0dee249ba09d3ccb380835baa93afdb2
Bajram Emini <bajram@eminiarts.ch>
2024-06-14 13:51:23 +0200

    Fixing Tests

--- tests/DatabaseMigrations/WithoutTeamsSchemaTest.php
+++ tests/DatabaseMigrations/WithoutTeamsSchemaTest.php
@@ -1,6 +1,10 @@
 <?php
 
+use Livewire\Livewire;
+use Aura\Base\Resources\Role;
 use Aura\Base\Resources\User;
+use Aura\Base\Resources\Option;
+use Aura\Base\Livewire\Settings;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -57,42 +61,64 @@ test('Aura without teams - options table', function () {
 });
 
 // Additional test for SettingsWithoutTeamsTest
-test('Settings Component can be rendered2', function () {
+test('Settings Component can be rendered', function () {
 
     $this->withoutExceptionHandling();
 
     expect(config('aura.features.theme_options'))->toBeTrue();
 
-    $user = User::find(1);
-
-    dd(DB::table('user_meta')->get());
-
-    ray($user->toArray())->purple();
-
-     ray(auth()->user()->toArray())->red();
-     ray(auth()->user()->roles)->red();
-
-        ray(auth()->user()->isSuperAdmin())->green();
-
-        expect(auth()->user()->isSuperAdmin())->toBeTrue();
+    expect(auth()->user()->isSuperAdmin())->toBeTrue();
 
     $response = $this->get(route('aura.settings'));
     $response->assertStatus(200);
 });
 
 test('Default Team Settings are created', function () {
-    $settings = config('team-settings');
-    expect($settings)->toBeArray();
+    // Default Team Settings
+    Livewire::test(Settings::class)
+        ->assertSee('Settings')
+        ->assertSee('primary')
+        ->assertSet('form.fields.darkmode-type', 'auto')
+        ->assertSet('form.fields.sidebar-type', 'primary')
+        ->assertSet('form.fields.color-palette', 'aura')
+        ->assertSet('form.fields.gray-color-palette', 'slate');
+
+    // assert DB has 1 record in options table
+    $this->assertDatabaseCount('options', 1);
+
+    // get first option from DB
+    $option = Option::first();
+
+    // assert option is team settings
+    // $this->assertEquals($option->name, 'settings');
+    $this->assertEquals($option->name, 'settings');
+
+    // assert $option->value is an array
+    $this->assertIsArray($option->value);
 });
 
 test('Settings can be saved', function () {
-    $this->post('/settings/save', [
-        'setting_name' => 'example_setting',
-        'setting_value' => 'example_value'
-    ])->assertStatus(200);
+    
+     // Default Team Settings
+    Livewire::test(Settings::class)
+        ->set('form.fields.darkmode-type', 'light')
+        ->set('form.fields.sidebar-type', 'light')
+        ->set('form.fields.color-palette', 'red')
+        ->set('form.fields.gray-color-palette', 'zinc')
+        ->call('save');
 
-    $this->assertDatabaseHas('settings', [
-        'name' => 'example_setting',
-        'value' => 'example_value'
-    ]);
+    // assert DB has 1 record in options table
+    $this->assertDatabaseCount('options', 1);
+
+    // get first option from DB
+    $option = Option::first();
+
+    // assert option is team settings
+    $this->assertEquals($option->name, 'settings');
+    $this->assertIsArray($option->value);
+
+    $this->assertEquals('light', $option->value['darkmode-type']);
+    $this->assertEquals('light', $option->value['sidebar-type']);
+    $this->assertEquals('red', $option->value['color-palette']);
+    $this->assertEquals('zinc', $option->value['gray-color-palette']);
 });
