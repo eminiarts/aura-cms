634bba8f60f4aa79c38196096f3aa984dc6114ba
Bajram Emini <bajram@eminiarts.ch>
2024-06-28 12:44:33 +0200

    Fix Modals

--- resources/views/components/confirms-action.blade.php
+++ resources/views/components/confirms-action.blade.php
@@ -1,4 +1,3 @@
-
 @props([
     'title' => __('Confirm Action'), 
     'content' => __('Are you sure you want to perform this action?'), 
@@ -10,55 +9,34 @@
     $confirmableId = md5($attributes->wire('then'));
 @endphp
 
-<div x-data="{ showModal: false }">
-<span
-    {{ $attributes->wire('then') }}
-    x-ref="span"
-    x-on:click="showModal = true"
-    x-on:action-confirmed.window="setTimeout(() => $event.detail.id === '{{ $confirmableId }}' && $refs.span.dispatchEvent(new CustomEvent('then', { bubbles: false })), 250); showModal = false;"
->
-    {{ $slot }}
-</span>
+<div {{ $attributes->wire('then') }} x-data="{ showModal: false }" x-on:action-confirmed.window="setTimeout(() => $event.detail.id === '{{ $confirmableId }}' && $dispatch('then'), 250); showModal = false;">
 
-{{-- @once --}}
-<x-aura::dialog wire:model="confirmingPassword">
+    <x-aura::dialog>
 
-    <span {{ $attributes->wire('then') }} x-data x-ref="span"
-        x-on:click="$wire.startConfirmingPassword('{{ $confirmableId }}')"
-        @password-confirmed.window="setTimeout(() => $event.detail.id === '{{ $confirmableId }}' && $refs.span.dispatchEvent(new CustomEvent('then', { bubbles: false })), 250);">
+    <x-aura::dialog.open>
+    <div>
         {{ $slot }}
-    </span>
+    </div>
+    </x-aura::dialog.open>
+    
+        <x-aura::dialog.panel>
+            <x-aura::dialog.title>{{ $title }}</x-aura::dialog.title>
 
-    <x-aura::dialog.panel>
-        <x-aura::dialog.title>{{ $title }}</x-aura::dialog.title>
+            <div class="mt-5 text-gray-600">
+                {!! $content !!}
+            </div>
 
-        <div>
-            {{ $content }}
-        </div>
+            <x-aura::dialog.footer>
+                <x-aura::dialog.close>
+                    <x-aura::button.transparent>
+                        {{ __('Cancel') }}
+                    </x-aura::button.transparent>
+                </x-aura::dialog.close>
 
-        <div class="mt-4" x-data="{}"
-            x-on:confirming-password.window="console.log('confirming password event fired....'); setTimeout(() => $refs.confirmable_password.focus(), 250)">
-            <x-aura::input type="password" class="mt-1 block w-3/4" placeholder="{{ __('Password') }}"
-                x-ref="confirmable_password" wire:model="confirmablePassword" wire:keydown.enter="confirmPassword" />
-
-            <x-aura::input-error for="confirmable_password" class="mt-2" />
-        </div>
-
-        <x-aura::dialog.footer>
-
-            <x-aura::dialog.close>
-                <x-aura::button.transparent>
-                    {{ __('Cancel') }}
-                </x-aura::button.transparent>
-            </x-aura::dialog.close>
-
-            <x-aura::button.primary class="ml-3" dusk="confirm-password-button" wire:click="confirmPassword"
-                wire:loading.attr="disabled">
-                {{ $button }}
-            </x-aura::button.primary>
-
-        </x-aura::dialog.footer>
-    </x-aura::dialog.panel>
-</x-aura::dialog>
-{{-- @endonce --}}
+                <x-aura::button.primary class="{{ $button_class }}" wire:click="confirmAction('{{ $confirmableId }}')" wire:loading.attr="disabled">
+                    {{ $button }}
+                </x-aura::button.primary>
+            </x-aura::dialog.footer>
+        </x-aura::dialog.panel>
+    </x-aura::dialog>
 </div>
