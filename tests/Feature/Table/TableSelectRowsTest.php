065e821d7cc0d377767a9c15e883f8de221dc475
Bajram Emini <bajram@eminiarts.ch>
2024-07-24 01:16:22 +0200

    Fixing Tests

--- tests/Feature/Table/TableSelectRowsTest.php
+++ tests/Feature/Table/TableSelectRowsTest.php
@@ -51,7 +51,7 @@ class TableSelectRowsModel extends Resource
                 'name' => 'Tags',
                 'slug' => 'tags',
                 'type' => 'Aura\\Base\\Fields\\Tags',
-                'model' => 'Aura\\Base\\Resources\\Tag',
+                'resource' => 'Aura\\Base\\Resources\\Tag',
                 'create' => true,
                 'validation' => '',
                 'conditional_logic' => [],
