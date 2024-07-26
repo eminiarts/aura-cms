7832d17ae93198e53bf154ee30bd768c1de3a43f
Bajram Emini <bajram@eminiarts.ch>
2024-07-26 23:23:32 +0200

    Update Sorting.php

--- src/Livewire/Table/Traits/Sorting.php
+++ src/Livewire/Table/Traits/Sorting.php
@@ -67,7 +67,8 @@ trait Sorting
                 return $query;
             }
 
-            if ($this->model->isMetaField($field)) {
+            if ($this->model->usesMeta() && $this->model->isMetaField($field)) {
+                ray('isMetaField');
                 $query->leftJoin('post_meta', function ($join) use ($field) {
                     $join->on('posts.id', '=', 'post_meta.post_id')
                         ->where('post_meta.key', '=', "$field");
