6f18b9f452cce10d1bf1c1220e720fd5dbdb80b1
Bajram Emini <bajram@eminiarts.ch>
2024-08-01 12:53:01 +0200

    Customize Component Command

--- src/AuraServiceProvider.php
+++ src/AuraServiceProvider.php
@@ -6,6 +6,7 @@ use Aura\Base\Commands\CreateAuraPlugin;
 use Aura\Base\Commands\CreateResourceFactory;
 use Aura\Base\Commands\CreateResourceMigration;
 use Aura\Base\Commands\CreateResourcePermissions;
+use Aura\Base\Commands\CustomizeComponent;
 use Aura\Base\Commands\DatabaseToResources;
 use Aura\Base\Commands\ExtendUserModel;
 use Aura\Base\Commands\InstallConfigCommand;
@@ -161,6 +162,7 @@ class AuraServiceProvider extends PackageServiceProvider
                 InstallConfigCommand::class,
                 MakeResource::class,
                 MakeUser::class,
+                CustomizeComponent::class,
                 CreateAuraPlugin::class,
                 MakeField::class,
                 PublishCommand::class,
