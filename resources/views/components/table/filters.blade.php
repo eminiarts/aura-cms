b1fc993d502bc01b6430eb86914ffeaf06b27dfa
Enes Emini <enes.emini@gmail.com>
2024-08-29 17:01:37 +0200

    WIP Filter

--- resources/views/components/table/filters.blade.php
+++ resources/views/components/table/filters.blade.php
@@ -5,7 +5,7 @@
     @endphp
     <div>
         {{-- select dropdown with filters @foreach($this->userFilters as $userFilter)--}}
-        <select wire:model="selectedFilter" class="block w-full px-3 py-2 pl-3 pr-10 mt-1 text-base bg-white border-gray-500/30 rounded-lg shadow-xs appearance-none focus:border-primary-300 focus:outline-none ring-gray-900/10 focus:ring focus:ring-primary-300 focus:ring-opacity-50 dark:focus:ring-primary-500 dark:focus:ring-opacity-50 dark:bg-gray-900 dark:border-gray-700 sm:text-sm">
+        <select wire:model="selectedFilter" class="block px-3 py-2 pr-10 pl-3 mt-1 w-full text-base bg-white rounded-lg appearance-none border-gray-500/30 shadow-xs focus:border-primary-300 focus:outline-none ring-gray-900/10 focus:ring focus:ring-primary-300 focus:ring-opacity-50 dark:focus:ring-primary-500 dark:focus:ring-opacity-50 dark:bg-gray-900 dark:border-gray-700 sm:text-sm">
             <option value="">
                 {{ __('Select a filter') }}
             </option>
@@ -16,7 +16,7 @@
 
         {{-- if a filter is selected show the filter --}}
         @if($this->selectedFilter)
-        <div class="flex items-center justify-between my-4">
+        <div class="flex justify-between items-center my-4">
             <h4 class="font-semibold text-primary-600">Filter: {{ $this->userFilters[$this->selectedFilter]['name'] }}</h4>
             <x-aura::button.transparent size="xs" wire:click="deleteFilter('{{ $this->selectedFilter }}')">
             <x-aura::icon icon="trash" size="xs"/> {{ __('Delete Filter') }}
@@ -67,28 +67,25 @@
                 </div>
             </div>
 
-            <div class="w-full mt-2 mb-0">
-                <select wire:model="filters.custom.{{ $key }}.name" id="filters_field_{{ $key}}" name="filters_field_{{ $key}}" class="block w-full py-2 pl-3 pr-10 mt-1 text-base border-gray-500/30 rounded-md focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm">
-                    @foreach($this->fieldsForFilter as $slug => $name)
-                    <option value="{{ $slug }}">{{ $name }}</option>
+            <div class="mt-2 mb-0 w-full">
+                <select wire:model="filters.custom.{{ $key }}.name" wire:change="$refresh" id="filters_field_{{ $key}}" name="filters_field_{{ $key}}" class="block py-2 pr-10 pl-3 mt-1 w-full text-base rounded-md border-gray-500/30 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm">
+                    @foreach($this->fieldsForFilter as $slug => $field)
+                    <option value="{{ $slug }}">{{ $field['name'] }}</option>
                     @endforeach
                 </select>
             </div>
 
-            <div class="w-full mt-2 mb-0">
-                <select wire:model="filters.custom.{{ $key }}.operator" id="filters_operator_{{ $key}}" name="filters_operator_{{ $key}}" class="block w-full py-2 pl-3 pr-10 mt-1 text-base border-gray-500/30 rounded-md focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm">
-                    <option value="contains">{{ __('contains') }}</option>
-                    <option value="does_not_contain">{{ __('does not contain') }}</option>
-                    <option value="is">{{ __('is') }}</option>
-                    <option value="is_not">{{ __('is not') }}</option>
-                    <option value="starts_with">{{ __('starts with') }}</option>
-                    <option value="ends_with">{{ __('ends with') }}</option>
-                    <option value="is_empty">{{ __('is empty') }}</option>
-                    <option value="is_not_empty">{{ __('is not empty') }}</option>
+            <div class="mt-2 mb-0 w-full">
+                <select wire:model="filters.custom.{{ $key }}.operator" id="filters_operator_{{ $key}}" name="filters_operator_{{ $key}}" class="block py-2 pr-10 pl-3 mt-1 w-full text-base rounded-md border-gray-500/30 focus:outline-none focus:ring-primary-500 focus:border-primary-500 sm:text-sm">
+                    @if(isset($filters['custom'][$key]['name']) && isset($this->fieldsForFilter[$filters['custom'][$key]['name']]))
+                        @foreach($this->fieldsForFilter[$filters['custom'][$key]['name']]['filterOptions'] as $operator => $label)
+                        <option value="{{ $operator }}">{{ $label }}</option>
+                        @endforeach
+                    @endif
                 </select>
             </div>
 
-            <div class="w-full mt-2 mb-0">
+            <div class="mt-2 mb-0 w-full">
                 <div class="w-full">
                     <x-aura::input.wrapper placeholder="Value" error="filters.custom.{{ $key }}.value">
                         <x-aura::input.text wire:model="filters.custom.{{ $key}}.value"></x-aura::input.text>
