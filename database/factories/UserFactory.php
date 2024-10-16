98df920bc55290af72bda159f282e5bb2f10d2f6
Bajram Emini <bajram@eminiarts.ch>
2024-10-16 13:46:55 +0200

    WIP

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,13 +2,19 @@
 
 namespace Aura\Base\Database\Factories;
 
+use Illuminate\Support\Str;
 use Aura\Base\Resources\Team;
 use Aura\Base\Resources\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
+    /**
+     * The current password being used by the factory.
+     */
+    protected static ?string $password;
+    
     /**
      * The name of the factory's corresponding model.
      *
@@ -27,7 +33,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
 
