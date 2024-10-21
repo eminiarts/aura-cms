0c3348dde0bd7ebc1aef0d4fdebca3b290375164
Bajram Emini <bajram@eminiarts.ch>
2024-10-21 09:27:30 +0200

    WIP

--- src/ConditionalLogic.php
+++ src/ConditionalLogic.php
@@ -100,6 +100,10 @@ class ConditionalLogic
 
         $cacheKey = md5(get_class($model).json_encode($field).json_encode($post).Auth::id());
 
+        if($field['slug'] === 'prompt') {
+            // ray($cacheKey, $post)->blue();
+        }
+
         return self::$shouldDisplayFieldCache[$cacheKey]
             ??= self::checkCondition($model, $field, $post);
     }
