b9f932cd429ad36089ec3394eff871ba9864561a
Bajram Emini <bajram@eminiarts.ch>
2024-10-29 20:38:04 +0100

    Fixing Tests

--- tests/Feature/ConfigTest.php
+++ tests/Feature/ConfigTest.php
@@ -9,23 +9,27 @@ beforeEach(function () {
 
 test('config features test', function () {
     expect(config('aura.features'))->toMatchArray([
-        'global_search' => true,
-        'notifications' => true,
+          'global_search' => true,
         'bookmarks' => true,
+        'last_visited_pages' => true,
+        'notifications' => true,
         'plugins' => true,
-        'flows' => false,
-        'forms' => true,
         'settings' => true,
         'profile' => true,
         'create_resource' => true,
         'resource_view' => true,
         'resource_edit' => true,
+        'resource_editor' => false,
+        'custom_tables_for_resources' => false,
     ]);
 });
 
-test('resource editor', function () {
-    expect(config('aura.resource_editor'))->toMatchArray([
-        'enabled' => config('app.env') == 'production' ? false : true,
-        'custom_table_migrations' => false,
+test('auth config', function () {
+    expect(config('aura.auth'))->toMatchArray([
+        'registration' => env('AURA_REGISTRATION', true),
+        'redirect' => '/admin',
+        '2fa' => true,
+        'user_invitations' => true,
+        'create_teams' => true,
     ]);
 });
