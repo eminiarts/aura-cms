7cfd12f5b10f24589b438e72fd8fa7ab10ac30ed
Bajram Emini <bajram@eminiarts.ch>
2024-10-31 17:55:50 +0100

    Update ApplyWrappers.php

--- src/Pipeline/ApplyWrappers.php
+++ src/Pipeline/ApplyWrappers.php
@@ -3,53 +3,51 @@
 namespace Aura\Base\Pipeline;
 
 use Closure;
+use Illuminate\Support\Str;
 
 class ApplyWrappers implements Pipe
 {
     public function handle($fields, Closure $next)
-    {
-        $fields = $this->processFields($fields);
-        return $next($fields);
-    }
-
-    private function processFields($fields)
     {
         $newFields = [];
         $i = 0;
         $fieldsCount = count($fields);
 
         while ($i < $fieldsCount) {
+            $wrapperFields = [];
             $field = $fields[$i];
 
-            // If wrap is true, start a new wrapper
-            if (isset($field['wrap']) && $field['wrap'] === true) {
-                $wrapperField = [
-                    'label'  => 'Wrapper',
-                    'name'   => 'Wrapper',
-                    'type'   => 'Wrapper',
-                ];
+            // Start a new wrapper
+            $wrapperFields[] = $field;
+            $i++;
 
-                // Collect consecutive fields with 'wrap' => true
-                while (
-                    $i < $fieldsCount &&
-                    isset($fields[$i]['wrap']) &&
-                    $fields[$i]['wrap'] === true
-                ) {
-                    $wrapperField['fields'][] = $fields[$i];
-                    $i++;
+            // Collect fields until we find a field with 'wrap' set to true
+            while ($i < $fieldsCount) {
+                if (isset($fields[$i]['wrap']) && $fields[$i]['wrap'] === true) {
+                    // Start a new wrapper in the next iteration
+                    break;
                 }
-
-                $newFields[] = $wrapperField;
-            } else {
-                // Process nested fields recursively if they exist
-                if (isset($field['fields']) && is_array($field['fields'])) {
-                    $field['fields'] = $this->processFields($field['fields']);
-                }
-                $newFields[] = $field;
+                $wrapperFields[] = $fields[$i];
                 $i++;
             }
+
+            $wrapperField = [
+                'label'  => 'Wrapper',
+                'name'   => 'Wrapper',
+                'type'   => 'Wrapper',
+                'fields' => $wrapperFields,
+                'slug'   => 'wrapper',
+            ];
+
+            $newFields[] = $wrapperField;
         }
 
-        return $newFields;
+        // Return a collection if the input was a collection
+        if ($fields instanceof \Illuminate\Support\Collection) {
+            $newFields = collect($newFields);
+        }
+
+        // Pass the new fields to the next pipe
+        return $next($newFields);
     }
 }
