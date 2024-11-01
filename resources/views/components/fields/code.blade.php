b52565d572351276083ee80547cd67bd02de92c4
Enes Emini <enes.emini@gmail.com>
2024-11-01 10:27:08 +0100

    Update code.blade.php

--- resources/views/components/fields/code.blade.php
+++ resources/views/components/fields/code.blade.php
@@ -1,15 +1,17 @@
 <x-aura::fields.wrapper :field="$field">
+    <?php $uniqueId = uniqid($field['slug'] . '_'); ?>
     <div class="overflow-hidden w-full bg-white rounded-lg border appearance-none border-gray-500/30 shadow-xs focus:border-primary-300 focus:outline-none ring-gray-900/10 focus:ring focus:ring-primary-300 focus:ring-opacity-50 dark:focus:ring-primary-500 dark:focus:ring-opacity-50 dark:bg-gray-900">
         <div>
             <div
                 x-data="{
                     editor: null,
-                    editorId: '{{ $field['slug'] }}'
+                    editorId: '{{ $uniqueId }}',
+                    fieldSlug: '{{ $field['slug'] }}'
                 }"
-                x-ref="editor_{{ $field['slug'] }}"
+                x-ref="editor_{{ $uniqueId }}"
                 x-init="
                     console.log('Initializing editor for: ' + editorId);
-                    this.editor = monaco.editor.create($refs['editor_' + editorId], {
+                    const editor = monaco.editor.create($refs['editor_' + editorId], {
                         value: @js($form['fields'][$field['slug']]),
                         language: '{{ $field['language'] ?? 'html' }}',
                         automaticLayout: true,
@@ -20,34 +22,30 @@
                             top: 16,
                             bottom: 16,
                         },
-                        scrollBeyondLastLine: false, // Disable scrolling beyond the last line
+                        scrollBeyondLastLine: false,
                         revealHorizontalRightPadding: 0,
-
-                        fontSize: 14, // Set the font size
-                        // fontFamily: 'Consolas, Courier New, monospace', // Set the font family
-                        lineHeight: 24, // Set the line height
-                        lineNumbers: '{{ optional($field)['line_numbers'] ? 'on' : 'off' }}', // Enable line numbers
-                        renderWhitespace: 'off', // Render whitespace characters
-                        wordWrap: 'on', // Enable word wrapping
-                        folding: true, // Enable code folding
-
-                        matchBrackets: 'always', // Highlight matching brackets
-
-                        autoIndent: 'full', // Enable auto indentation
-                        smoothScrolling: true, // Enable smooth scrolling
+                        fontSize: 14,
+                        lineHeight: 24,
+                        lineNumbers: '{{ optional($field)['line_numbers'] ? 'on' : 'off' }}',
+                        renderWhitespace: 'off',
+                        wordWrap: 'on',
+                        folding: true,
+                        matchBrackets: 'always',
+                        autoIndent: 'full',
+                        smoothScrolling: true,
                         scrollbar: {
-                            vertical: 'visible', // Always show vertical scrollbar
-                            horizontal: 'visible', // Always show horizontal scrollbar
+                            vertical: 'visible',
+                            horizontal: 'visible',
                         },
-                        suggestOnTriggerCharacters: true, // Enable IntelliSense suggestions on trigger characters
+                        suggestOnTriggerCharacters: true,
                         quickSuggestions: {
                             other: true,
                             comments: true,
                             strings: true
                         },
-                        renderIndentGuides: false, // Disable rendering of indent guides
+                        renderIndentGuides: false,
                         guides: {
-                            indentation: false // Disable indentation guides
+                            indentation: false
                         }
                     });
 
@@ -57,11 +55,10 @@
                     }
 
                     // Listen for changes in the editor and update Livewire model
-                    this.editor.onDidChangeModelContent(() => {
-                        content = this.editor.getValue();
-                        console.log(this.editor);
+                    editor.onDidChangeModelContent(() => {
+                        const content = editor.getValue();
                         console.log('content for ' + editorId + ': ', content);
-                        $wire.$set('form.fields.' + editorId, content);
+                        $wire.$set('form.fields.' + fieldSlug, content);
                     });
                 "
                 wire:ignore
