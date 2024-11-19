76840b1d901e847edeb25943ef5a553381c5e22d
Bajram Emini <bajram@eminiarts.ch>
2024-11-19 15:39:42 +0100

    Update ComplexFieldsTest.php

--- tests/Feature/Fields/ComplexFieldsTest.php
+++ tests/Feature/Fields/ComplexFieldsTest.php
@@ -106,6 +106,7 @@ class ComplexFieldsTestModel extends Resource
                 'validation' => '',
                 'conditional_logic' => [],
                 'wrapper' => '',
+                'exclude_level' => 2,
             ],
             [
                 'name' => 'Text 2',
@@ -186,6 +187,8 @@ test('complex fields are possible and working correctly', function () {
     $fields = $model->getGroupedFields();
 
     ray($fields);
+
+    // ray(json_encode($fields));
 
     $this->assertCount(1, $fields);
     $this->assertEquals($fields[0]['name'], 'Panel');
