0acfbccc3abf43ee233f27779d9c02b57ee10a20
Bajram Emini <bajram@eminiarts.ch>
2024-11-26 21:43:00 +0100

    WIP

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -14,42 +14,49 @@
         x-on:click.prevent="$dispatch('open-modal', { component: 'confirm-user-deletion' })"
     >{{ __('Delete Account') }}</x-aura::danger-button>
 
-    <x-aura::modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
-        <form method="post" action="{{ route('aura.profile.destroy') }}" class="p-6">
-            @csrf
-            @method('delete')
-
-            <h2 class="text-lg font-medium text-gray-900">
-                {{ __('Are you sure you want to delete your account?') }}
-            </h2>
-
-            <p class="mt-1 text-sm text-gray-600">
-                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
-            </p>
-
-            <div class="mt-6">
-                <x-aura::input-label for="password" value="Password" class="sr-only" />
-
-                <x-aura::text-input
-                    id="password"
-                    name="password"
-                    type="password"
-                    class="mt-1 block w-3/4"
-                    placeholder="Password"
-                />
-
-                <x-aura::input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+    <x-aura::dialog wire:model="showDeleteModal">
+        <x-aura::dialog.open>
+            <div x-data="" x-on:click="$dispatch('open-modal', { component: 'confirm-user-deletion' })">
+                {{ $slot }}
             </div>
+        </x-aura::dialog.open>
 
-            <div class="mt-6 flex justify-end">
-                <x-aura::secondary-button x-on:click="$dispatch('close')">
-                    {{ __('Cancel') }}
-                </x-aura::secondary-button>
+        <x-aura::dialog.panel>
+            <form method="post" action="{{ route('aura.profile.destroy') }}">
+                @csrf
+                @method('delete')
 
-                <x-aura::danger-button class="ml-3">
-                    {{ __('Delete Account') }}
-                </x-aura::danger-button>
-            </div>
-        </form>
-    </x-aura::modal>
+                <x-aura::dialog.title>
+                    {{ __('Are you sure you want to delete your account?') }}
+                </x-aura::dialog.title>
+
+                <div class="mt-5 text-gray-600">
+                    {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+
+                    <div class="mt-6">
+                        <x-aura::input.wrapper label="Password" error="password">
+                            <x-aura::input.text
+                                type="password"
+                                name="password"
+                                error="password"
+                                placeholder="Password"
+                            />
+                        </x-aura::input.wrapper>
+                    </div>
+                </div>
+
+                <x-aura::dialog.footer>
+                    <x-aura::dialog.close>
+                        <x-aura::button.transparent>
+                            {{ __('Cancel') }}
+                        </x-aura::button.transparent>
+                    </x-aura::dialog.close>
+
+                    <x-aura::button.danger type="submit">
+                        {{ __('Delete Account') }}
+                    </x-aura::button.danger>
+                </x-aura::dialog.footer>
+            </form>
+        </x-aura::dialog.panel>
+    </x-aura::dialog>
 </section>
