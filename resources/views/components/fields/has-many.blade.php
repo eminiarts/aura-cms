34ff2fe26e1587d71efa6c8c494fc2b17ff79843
Bajram Emini <bajram@eminiarts.ch>
2024-11-26 16:10:05 +0100

    WIP

--- resources/views/components/fields/has-many.blade.php
+++ resources/views/components/fields/has-many.blade.php
@@ -1,6 +1,6 @@
 @checkCondition($this->model, $field)
     <div class="w-full px-2 {{ $field['style']['class'] ?? '' }}">
         {{-- <livewire:aura::table :field="$field" :editInModal="true" :namespace="$field['resource']"
-            :createInModal="true" :parent="$model->setRelations([])" /> --}}
+             :parent="$model->setRelations([])" /> --}}
     </div>
 @endcheckCondition
