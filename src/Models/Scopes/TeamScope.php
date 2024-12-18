e10113e9d4d3185cf8de52d8559b03d1ae7e5e9c
Bajram Emini <bajram@eminiarts.ch>
2024-12-18 11:44:47 +0100

    WIP

--- src/Models/Scopes/TeamScope.php
+++ src/Models/Scopes/TeamScope.php
@@ -2,6 +2,7 @@
 
 namespace Aura\Base\Models\Scopes;
 
+use Aura\Base\Resources\Team;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
@@ -15,46 +16,46 @@ class TeamScope implements Scope
      */
     public function apply(Builder $builder, Model $model)
     {
+
         if (config('aura.teams') === false) {
             return $builder;
         }
 
-        // return $builder;
+        $userClass = app(config('aura.resources.user'));
 
+        // Prevent infinite loop by not applying scope to User model
+        if ($model instanceof $userClass) {
+            return $builder;
+        }
+        
         // If the Model is a Team Resource, don't apply the scope
         $teamClass = app(config('aura.resources.team'));
         
-        // if (auth()->user() && $model instanceof $teamClass) {
-        //     return $builder->whereId(auth()->user()->current_team_id);
-        // }
-
-        // if (auth()->user() && $model instanceof \Aura\Base\Resources\Role) {
-        //     return $builder->where('posts.team_id', auth()->user()->current_team_id);
-        // }
-
-       // return $builder;
-
-        // if (auth()->user() && $model->getTable() == 'posts') {
-        //     return $builder->where('posts.team_id', auth()->user()->current_team_id);
-        // }
-
-        // if(auth()->guest()) {
-        //     return $builder;
-        // }
-
-        return $builder;
-
-    dd(auth()->user()->current_team_id);
-
-        if (auth()->user() && $model->getTable() == 'posts') {
-            return $builder->where($model->getTable().'.team_id', auth()->user()->current_team_id);
+        if (auth()->user() && $model instanceof $teamClass) {
+            // For Now
+            return $builder;
+            // return $builder->whereId(auth()->user()->current_team_id);
         }
 
-        if (auth()->user()) {
-            return $builder->where($model->getTable().'.team_id', auth()->user()->current_team_id);
+        // Get user without triggering scopes
+        $user = auth()->getUser();
+        
+        if (!$user) {
+            return $builder;
         }
 
-        // Check access?
-        return $builder;
+        // Get the current_team_id directly from the user attributes to avoid scope
+        $currentTeamId = $user->getAttribute('current_team_id');
+        
+        if ($model->getTable() == 'posts') {
+            return $builder->where($model->getTable().'.team_id', $currentTeamId);
+        }
+
+        // Temporary Fix
+        if($model->getTable() == 'teams') {
+            return $builder;
+        }
+
+        return $builder->where($model->getTable().'.team_id', $currentTeamId);
     }
 }
