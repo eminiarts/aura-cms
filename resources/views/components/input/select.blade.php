faee0a1a354c9741dc660e58d60d4f50b62f289b
Bajram Emini <bajram@eminiarts.ch>
2024-12-25 14:30:39 +0100

    Update select.blade.php

--- resources/views/components/input/select.blade.php
+++ resources/views/components/input/select.blade.php
@@ -41,10 +41,4 @@
             @endif
         @endforeach
     </select>
-
-    <div class="flex absolute inset-y-0 right-0 items-center {{ $size === 'xs' ? 'px-1' : 'px-2' }} pointer-events-none">
-        <svg class="{{ $size === 'xs' ? 'w-4 h-4' : 'w-5 h-5' }} text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 15L12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
-        </svg>
-    </div>
 </div>
