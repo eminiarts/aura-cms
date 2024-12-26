b4951a988e9440be61f5eca3742af500b452abe8
Bajram Emini <bajram@eminiarts.ch>
2024-12-26 23:29:18 +0100

    Improved Uploader

--- resources/views/livewire/attachment/index.blade.php
+++ resources/views/livewire/attachment/index.blade.php
@@ -1,26 +1,6 @@
 <div>
 
-    <div class="flex justify-between items-start">
 
-    <div class="mb-6">
-        <x-aura::breadcrumbs>
-            <x-aura::breadcrumbs.li :href="route('aura.dashboard')" title="" icon="dashboard" iconClass="text-gray-500 w-6 h-6 mr-0" />
-            <x-aura::breadcrumbs.li :title="Str::plural($slug)"  />
-        </x-aura::breadcrumbs.li>
-    </div>
-
-    <div>
-         @if (config('aura.features.resource_editor'))
-                    @if ($resource->isAppResource())
-                        <x-aura::button.transparent :href="route('aura.resource.editor', $slug)" size="">
-                            <x-aura::icon icon="cog" class="mr-2" />
-                            {{ __('Edit Resource') }}
-                        </x-aura::button.transparent>
-                    @endif
-            @endif
-    </div>
-
-    </div>
 
     <div wire:key="widgets">
         @if($widgets = $resource->getWidgets())
