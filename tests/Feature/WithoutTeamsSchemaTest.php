123c628b5ae0d387f3828fa11f0ec5d9306ff88f
Bajram Emini <bajram@eminiarts.ch>
2025-01-07 10:14:27 +0100

    Update WithoutTeamsSchemaTest.php

--- tests/Feature/WithoutTeamsSchemaTest.php
+++ tests/Feature/WithoutTeamsSchemaTest.php
@@ -1,12 +1,17 @@
 <?php
 
+namespace Tests\Feature;
+
 use Aura\Base\Livewire\Settings;
 use Aura\Base\Resources\Option;
+use Aura\Base\Resources\Role;
 use Aura\Base\Resources\User;
+use Aura\Base\Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Livewire\Livewire;
+
 
 beforeAll(function () {
     putenv('AURA_TEAMS=false');
@@ -18,18 +23,30 @@ afterAll(function () {
 
 // Before each test, create a Superadmin and login
 beforeEach(function () {
-    $this->actingAs($this->user = createSuperAdminWithoutTeam());
+    $user = User::factory()->create();
+
+    $role = Role::create([
+        'type' => 'Role',
+        'title' => 'Super Admin',
+        'slug' => 'super_admin',
+        'name' => 'Super Admin',
+        'description' => 'Super Admin can perform everything.',
+        'super_admin' => true,
+        'permissions' => [],
+        'user_id' => $user->id,
+    ]);
+
+    $user->update(['roles' => [$role->id]]);
+    $user->refresh();
+
+    $this->actingAs($user);
 });
 
 test('Aura without teams', function () {
     expect(config('aura.teams'))->toBeFalse();
 
-    // Refresh the database using the RefreshDatabase trait
-    $this->refreshTestDatabase();
-
     // expect teams table not to exist
     expect(Schema::hasTable('teams'))->toBeFalse();
-    // expect(Schema::hasTable('user_role'))->toBeFalse();
     expect(Schema::hasTable('team_meta'))->toBeFalse();
 });
 
@@ -38,9 +55,6 @@ test('Aura without teams - table columns', function () {
     config(['aura.teams' => false]);
 
     expect(config('aura.teams'))->toBeFalse();
-
-    // Refresh the database using the RefreshDatabase trait
-    $this->refreshTestDatabase();
 
     // expect user table not to have current_team_id
     expect(Schema::hasColumn('users', 'current_team_id'))->toBeFalse();
@@ -58,11 +72,11 @@ test('Aura without teams - options table', function () {
 
 // Additional test for SettingsWithoutTeamsTest
 test('Settings Component can be rendered', function () {
-
+    config(['aura.features.settings' => true]);
+    
     $this->withoutExceptionHandling();
 
     expect(config('aura.features.settings'))->toBeTrue();
-
     expect(auth()->user()->isSuperAdmin())->toBeTrue();
 
     $response = $this->get(route('aura.settings'));
@@ -70,6 +84,8 @@ test('Settings Component can be rendered', function () {
 });
 
 test('Default Team Settings are created', function () {
+    config(['aura.features.settings' => true]);
+    
     // Default Team Settings
     Livewire::test(Settings::class)
         ->assertSee('Settings')
@@ -86,7 +102,6 @@ test('Default Team Settings are created', function () {
     $option = Option::first();
 
     // assert option is team settings
-    // $this->assertEquals($option->name, 'settings');
     $this->assertEquals($option->name, 'settings');
 
     // assert $option->value is an array
@@ -94,14 +109,19 @@ test('Default Team Settings are created', function () {
 });
 
 test('Settings can be saved', function () {
-
+    config(['aura.features.settings' => true]);
+    
     // Default Team Settings
     Livewire::test(Settings::class)
         ->set('form.fields.darkmode-type', 'light')
         ->set('form.fields.sidebar-type', 'light')
-        ->set('form.fields.color-palette', 'red')
+        ->set('form.fields.color-palette', 'rose')
         ->set('form.fields.gray-color-palette', 'zinc')
-        ->call('save');
+        ->call('save')
+        ->assertSet('form.fields.darkmode-type', 'light')
+        ->assertSet('form.fields.sidebar-type', 'light')
+        ->assertSet('form.fields.color-palette', 'rose')
+        ->assertSet('form.fields.gray-color-palette', 'zinc');
 
     // assert DB has 1 record in options table
     $this->assertDatabaseCount('options', 1);
@@ -111,10 +131,13 @@ test('Settings can be saved', function () {
 
     // assert option is team settings
     $this->assertEquals($option->name, 'settings');
+
+    // assert $option->value is an array
     $this->assertIsArray($option->value);
 
-    $this->assertEquals('light', $option->value['darkmode-type']);
-    $this->assertEquals('light', $option->value['sidebar-type']);
-    $this->assertEquals('red', $option->value['color-palette']);
-    $this->assertEquals('zinc', $option->value['gray-color-palette']);
+    // assert settings are saved
+    $this->assertEquals($option->value['darkmode-type'], 'light');
+    $this->assertEquals($option->value['sidebar-type'], 'light');
+    $this->assertEquals($option->value['color-palette'], 'rose');
+    $this->assertEquals($option->value['gray-color-palette'], 'zinc');
 });
