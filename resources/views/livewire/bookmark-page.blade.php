851d2790cc956403c7dad539109296767a7ee16b
Bajram Emini <bajram@eminiarts.ch>
2025-03-02 22:54:59 +0100

    Update bookmark-page.blade.php

--- resources/views/livewire/bookmark-page.blade.php
+++ resources/views/livewire/bookmark-page.blade.php
@@ -1,13 +1,17 @@
 <div class="text-gray-300">
     @if(config('aura.features.bookmarks') && config('aura.features.global_search'))
-    <button wire:click="toggleBookmark" class="flex justify-center items-center ml-2 w-6 h-6 focus:outline-none" :class="{'text-primary-600': {{ $this->isBookmarked }}, 'text-gray-300': !{{ $this->isBookmarked }} }">
+    <div x-data="{ isBookmarked: {{ $this->isBookmarked ? 'true' : 'false' }} }">
+        <button wire:click="toggleBookmark" 
+                class="flex justify-center items-center ml-2 w-6 h-6 focus:outline-none" 
+                :class="{'text-primary-600': isBookmarked, 'text-gray-300': !isBookmarked }">
 
-        @if(!$this->isBookmarked)
-            <x-aura::icon.bookmark class="w-5 h-5" />
-        @else
-            <x-aura::icon.bookmarked class="w-5 h-5" />
-        @endif
+            @if(!$this->isBookmarked)
+                <x-aura::icon.bookmark class="w-5 h-5" />
+            @else
+                <x-aura::icon.bookmarked class="w-5 h-5" />
+            @endif
 
-    </button>
+        </button>
+    </div>
     @endif
 </div>
