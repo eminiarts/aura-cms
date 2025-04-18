f94f3b9198ed5e991537408c4af62a171160c97d
Bajram Emini <bajram@eminiarts.ch>
2025-04-18 12:19:33 +0200

    Update modal UI and add wire:key for better Livewire reactivity

--- resources/views/livewire/modals.blade.php
+++ resources/views/livewire/modals.blade.php
@@ -3,16 +3,21 @@
         <x-aura::dialog wire:model="activeModals.{{ $id }}">
 
             @if ($modal['modalAttributes']['slideOver'])
-                <x-aura::dialog.slideover>
+                <x-aura::dialog.slideover wire:key="slideover-{{ $id }}">
                     {{-- @dump($activeModals) --}}
                     @livewire($modal['name'], $modal['arguments'], key($id))
                 </x-aura::dialog.slideover>
             @else
-                <x-aura::dialog.panel :modalAttributes="$modal['modalAttributes']">
+                <x-aura::dialog.panel :modalAttributes="$modal['modalAttributes']" wire:key="panel-{{ $id }}">
                     @livewire($modal['name'], array_merge($modal['arguments'], ['modalAttributes' => $modal['modalAttributes']]), key($id))
                 </x-aura::dialog.panel>
             @endif
         </x-aura::dialog>
     @empty
     @endforelse
+
+    @php
+        ray('modals');
+        ray($modals);
+    @endphp
 </div>
