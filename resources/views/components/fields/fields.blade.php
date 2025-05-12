42db3035ccf270d201b726ffe87582301e0c601c
Bajram Emini <bajram@eminiarts.ch>
2025-05-12 22:39:40 +0200

    Update fields.blade.php

--- resources/views/components/fields/fields.blade.php
+++ resources/views/components/fields/fields.blade.php
@@ -1,6 +1,6 @@
 @aware(['field', 'model', 'form', 'mode'])
 
-<div class="flex flex-wrap items-start -mx-2 w-full" wire:key="tab-field-{{ md5(json_encode($field)) }}">
+<div class="flex flex-wrap items-start -mx-2 w-full">
     @if(optional($field)['fields'])
         @foreach($field['fields'] as $key => $field)
             @checkCondition($this->model ?? $model, $field, $this->form ?? $form)
