639fb3d40e2c3682ff3eca05310a8b70e141dee9
bajramemini <3426944+bajramemini@users.noreply.github.com>
2025-08-07 07:50:12 +0000

    Fix styling

--- src/Livewire/Resource/CreateModal.php
+++ src/Livewire/Resource/CreateModal.php
@@ -4,19 +4,19 @@ namespace Aura\Base\Livewire\Resource;
 
 class CreateModal extends Create
 {
+    public static function modalMaxWidth(): string
+    {
+        return '7xl';
+    }
+
     public function mount($slug = null, $params = [])
     {
         $this->inModal = true;
         $this->params = $params;
-        
+
         ray('CreateModal mount called with:', ['slug' => $slug, 'params' => $params])->orange();
 
         parent::mount($slug);
-    }
-
-    public static function modalMaxWidth(): string
-    {
-        return '7xl';
     }
 
     public function render()
