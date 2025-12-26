575d6851089acb8c356ea85236d3f07175aeec9e
Bajram Emini <bajram@eminiarts.ch>
2025-12-26 23:38:36 +0100

    Fix unclosed livewire tag in edit-modal.blade.php
    Livewire dev-main requires proper self-closing tags for livewire components.

--- resources/views/livewire/resource/edit-modal.blade.php
+++ resources/views/livewire/resource/edit-modal.blade.php
@@ -1,6 +1,6 @@
 <div class="">
     @if(isset($model) && $model)
-        <livewire:aura::resource-edit :slug="$type" :id="$resource" :inModal="true">
+        <livewire:aura::resource-edit :slug="$type" :id="$resource" :inModal="true" />
     @else
         <div class="p-4 text-center text-gray-500">
             {{ __('Resource not found') }}
