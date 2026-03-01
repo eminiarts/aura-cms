f448ea0a611c526f8f149d72f212ac67ed4adebb
Bajram Emini <eminiarts@Mini-von-Bajram.localdomain>
2026-03-01 23:17:03 +0100

    Fix invite user modal: replace x-dialog with simple Alpine x-show modal
    - Replace <x-aura::dialog> with inline Alpine modal using @entangle
    - Use x-show + x-transition instead of x-dialog/x-teleport
    - Fixed z-index as inline styles (99 overlay, 100 panel)
    - Sidebar stays visible behind semi-transparent backdrop
    - Cancel button dispatches close-invite-modal browser event
    - Escape key closes modal
    - Validation errors keep modal open (already handled by Livewire)

--- resources/views/livewire/user/invite-user.blade.php
+++ resources/views/livewire/user/invite-user.blade.php
@@ -1,26 +1,21 @@
-<div class="">
-
+<div class="p-6">
     <form wire:submit="save">
-        <x-aura::dialog.title>{{ __('Invite User') }}</x-aura::dialog.title>
+        <h2 class="text-lg font-semibold text-gray-900 dark:text-white mb-4">{{ __('Invite User') }}</h2>
 
         <div class="flex flex-wrap">
-
             <div class="w-full">
                 <div class="flex flex-wrap items-start -mx-4 mb-4">
                     @foreach ($this->fields as $key => $field)
-
-                            <x-dynamic-component :component="$field['field']->edit()" mode="edit" :field="$field" :form="$form" />
+                        <x-dynamic-component :component="$field['field']->edit()" mode="edit" :field="$field" :form="$form" />
                     @endforeach
                 </div>
             </div>
         </div>
 
-        <x-aura::dialog.footer>
-            <x-aura::dialog.close>
-                <x-aura::button.transparent>
-                    {{ __('Cancel') }}
-                </x-aura::button.transparent>
-            </x-aura::dialog.close>
+        <div class="flex justify-end gap-3 mt-4">
+            <x-aura::button.transparent type="button" x-on:click="$dispatch('close-invite-modal')">
+                {{ __('Cancel') }}
+            </x-aura::button.transparent>
 
             <x-aura::button.primary type="submit">
                 <div wire:loading wire:target="save">
@@ -28,8 +23,6 @@
                 </div>
                 {{ __('Invite') }}
             </x-aura::button.primary>
-        </x-aura::dialog.footer>
-
+        </div>
     </form>
-
 </div>
