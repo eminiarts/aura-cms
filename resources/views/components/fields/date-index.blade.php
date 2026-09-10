ec4d955e64dd924103da12700ee8ef8a63edb25f
Bajram Emini <bajram@eminiarts.ch>
2026-09-10 14:58:07 +0200

    fix: leave missing dates empty in resource views

--- resources/views/components/fields/date-index.blade.php
+++ resources/views/components/fields/date-index.blade.php
@@ -1,8 +1,10 @@
+@if($value === null || $value === '')
+    <span class="text-gray-400">–</span>
+@else
     @php
         if (isset($field['display_format'])) {
             $value = \Carbon\Carbon::parse($value)->format($field['display_format']);
         }
     @endphp
-    <span class="">
-        {{ $value }}
-    </span>
+    <span>{{ $value }}</span>
+@endif
